63f42cb07e8fcb47d9f6f5105abc035446dbb3c3
Victor <katashiroku@users.noreply.github.com>
2018-09-13 21:31:37 +0300

    Add files via upload

--- chrome/popup/js/menu.js
+++ chrome/popup/js/menu.js
@@ -595,20 +595,40 @@ var menu = {
             default: 'true'
           },
           {
-            label: '0',
-            value: '0'
+            label: '10',
+            value: '10'
           },
           {
-            label: '25',
-            value: '25'
+            label: '20',
+            value: '20'
+          },
+          {
+            label: '30',
+            value: '30'
+          },
+          {
+            label: '40',
+            value: '40'
           },
           {
             label: '50',
             value: '50'
           },
           {
-            label: '75',
-            value: '75'
+            label: '60',
+            value: '60'
+          },
+          {
+            label: '70',
+            value: '70'
+          },
+          {
+            label: '80',
+            value: '80'
+          },
+          {
+            label: '90',
+            value: '90'
           },
           {
             label: '100',
