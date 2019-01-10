6f1ca7c55b604252ab15e5d462dd9cb6c050ab30
Victor <katashiroku@users.noreply.github.com>
2019-01-11 02:10:26 +0300

    1.9.3

--- chrome/youtube/js/category-playlist.js
+++ chrome/youtube/js/category-playlist.js
@@ -13,7 +13,42 @@
 function playlist_reverse() {
   const data = settings.playlist_reverse;
 
-  if (data == 'true') {
+  if (!getUrlParams().hasOwnProperty('list'))
+    return false;
+
+  if (data == 'true' && document.querySelector('.ytp-left-controls > a:first-child') && document.documentElement.getAttribute('youtube-version') == 'old') {
+    let prev_href_0 = document.querySelector('div.playlist-behavior-controls a.prev-playlist-list-item') ? document.querySelector('div.playlist-behavior-controls a.prev-playlist-list-item').getAttribute('href') : null,
+      next_href_0 = document.querySelector('div.playlist-behavior-controls a.next-playlist-list-item') ? document.querySelector('div.playlist-behavior-controls a.next-playlist-list-item').getAttribute('href') : null,
+      prev_href = document.querySelector('.ytp-left-controls > a:first-child').getAttribute('href'),
+      prev_title = document.querySelector('.ytp-left-controls > a:first-child').getAttribute('data-tooltip-text'),
+      prev_preview = document.querySelector('.ytp-left-controls > a:first-child').getAttribute('data-preview'),
+      next_href = document.querySelector('.ytp-left-controls > a:not(:first-child)').getAttribute('href'),
+      next_title = document.querySelector('.ytp-left-controls > a:not(:first-child)').getAttribute('data-tooltip-text'),
+      next_preview = document.querySelector('.ytp-left-controls > a:not(:first-child)').getAttribute('data-preview');
+
+    document.querySelector('.ytp-left-controls > a:first-child').setAttribute('href', next_href);
+    document.querySelector('.ytp-left-controls > a:first-child').setAttribute('data-tooltip-text', next_title);
+    document.querySelector('.ytp-left-controls > a:first-child').setAttribute('data-preview', next_preview);
+    document.querySelector('.ytp-left-controls > a:not(:first-child)').setAttribute('href', prev_href);
+    document.querySelector('.ytp-left-controls > a:not(:first-child)').setAttribute('data-tooltip-text', prev_title);
+    document.querySelector('.ytp-left-controls > a:not(:first-child)').setAttribute('data-preview', prev_preview);
+
+
+    if (document.documentElement.getAttribute('youtube-version') == 'old') {
+      document.querySelector('div.playlist-behavior-controls a.prev-playlist-list-item').setAttribute('href', next_href_0);
+      document.querySelector('div.playlist-behavior-controls a.next-playlist-list-item').setAttribute('href', prev_href_0);
+
+      let playlist = document.querySelectorAll('.playlist-videos-container > ol > li');
+
+      document.querySelector('.playlist-videos-container > ol').innerHTML = '';
+
+      for (let i = playlist.length - 1; i > 0; i--) {
+        document.querySelector('.playlist-videos-container > ol').appendChild(playlist[i]);
+      }
+    }
+  }
+
+  if (data == 'true' && document.documentElement.getAttribute('youtube-version') == 'new') {
     if (document.querySelector('ytd-app')) {
       var wait = setInterval(function () {
         if (document.querySelector('ytd-app').data) {
@@ -49,7 +84,6 @@ function playlist_reverse() {
         }
       }, 15);
     }
-
   }
 }
 
@@ -59,7 +93,7 @@ function playlist_reverse() {
 --------------------------------------------------------------*/
 
 function playlist_repeat() {
-  setInterval(function () {
+  setInterval(function() {
     if (
       document.querySelectorAll('#playlist-actions #top-level-buttons ytd-toggle-button-renderer')[0] ||
       document.querySelector('.playlist-nav-controls .toggle-loop')
@@ -68,7 +102,7 @@ function playlist_repeat() {
 
       if (data) {
         let new_youtube_toggle = document.querySelectorAll('#playlist-actions #top-level-buttons ytd-toggle-button-renderer'),
-            old_youtube_toggle = document.querySelector('.playlist-nav-controls .toggle-loop');
+          old_youtube_toggle = document.querySelector('.playlist-nav-controls .toggle-loop');
 
         if (new_youtube_toggle[0] && (data == 'enabled' && new_youtube_toggle[0].className.search('style-default-active') == -1 || data == 'disabled' && new_youtube_toggle[0].className.search('style-default-active') != -1))
           new_youtube_toggle[0].click();
@@ -86,7 +120,7 @@ function playlist_repeat() {
 --------------------------------------------------------------*/
 
 function playlist_shuffle() {
-  setInterval(function () {
+  setInterval(function() {
     if (
       document.querySelectorAll('#playlist-actions #top-level-buttons ytd-toggle-button-renderer')[1] ||
       document.querySelector('.playlist-nav-controls .shuffle-playlist')
@@ -95,7 +129,7 @@ function playlist_shuffle() {
 
       if (data && data != 'true') {
         let new_youtube_toggle = document.querySelectorAll('#playlist-actions #top-level-buttons ytd-toggle-button-renderer'),
-            old_youtube_toggle = document.querySelector('.playlist-nav-controls .shuffle-playlist');
+          old_youtube_toggle = document.querySelector('.playlist-nav-controls .shuffle-playlist');
 
         if (new_youtube_toggle[1] && (data == 'enabled' && new_youtube_toggle[1].className.search('style-default-active') == -1 || data == 'disabled' && new_youtube_toggle[1].className.search('style-default-active') != -1))
           new_youtube_toggle[1].click();
