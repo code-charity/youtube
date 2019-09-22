71d3e4ea4ea00b7fdde553b60b0db06fe21281de
Victor Savinov <katashiroku@users.noreply.github.com>
2019-09-22 15:35:43 +0300

    Update injection.js

--- firefox (v1.50)/youtube/js/injection.js
+++ firefox (v1.50)/youtube/js/injection.js
@@ -1,1 +1,48 @@
+/*-----------------------------------------------------------------------------
+>>> INJECTION
+-------------------------------------------------------------------------------
+1.0 Storage
+2.0 Message listener
+-----------------------------------------------------------------------------*/
 
+/*-----------------------------------------------------------------------------
+1.0 Storage
+-----------------------------------------------------------------------------*/
+
+chrome.storage.local.get(function(items) {
+    var inject = 'var ImprovedTube={';
+
+    inject += 'storage:' + JSON.stringify(items);
+
+    for (var key in items) {
+        var name = key;
+
+        document.documentElement.setAttribute('it-' + name.replace(/_/g, '-'), items[key]);
+    }
+
+    for (var key in ImprovedTube) {
+        inject += ',' + key + ':' + ImprovedTube[key];
+    }
+
+    inject += '};ImprovedTube.init();';
+
+    injectScript(inject);
+});
+
+
+/*-----------------------------------------------------------------------------
+2.0 Message listener
+-----------------------------------------------------------------------------*/
+
+chrome.runtime.onMessage.addListener(function(request) {
+    var name = request.name,
+        value = request.value;
+
+    document.documentElement.setAttribute('it-' + name.replace(/_/g, '-'), value);
+
+    injectScript('ImprovedTube.storage[\'' + name + '\']=\'' + value + '\';');
+
+    if (typeof ImprovedTube[name] === 'function') {
+        injectScript('ImprovedTube.' + name + '();');
+    }
+});
