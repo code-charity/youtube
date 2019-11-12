b7f36c4df5bb10938bd1cd119e455e45eccaa3e5
Victor Savinov <katashiroku@gmail.com>
2019-11-12 14:14:50 +0300

    2.0.272

--- background.js
+++ background.js
@@ -287,3 +287,22 @@ chrome.storage.local.get(function(items) {
 -----------------------------------------------------------------------------*/
 
 chrome.runtime.setUninstallURL('http://improvedtube.com/uninstalled');
+
+if (chrome.runtime.getManifest()
+    .version == chrome.runtime.getManifest()
+    .version_name) {
+    var _gaq = _gaq || [];
+
+    _gaq.push(['_setAccount', 'UA-88354155-1']);
+    _gaq.push(['_setSessionCookieTimeout', 14400000]);
+
+    (function() {
+        let ga = document.createElement('script'),
+            s = document.getElementsByTagName('script')[0];
+
+        ga.type = 'text/javascript';
+        ga.async = true;
+        ga.src = 'https://ssl.google-analytics.com/ga.js';
+        s.parentNode.insertBefore(ga, s);
+    })();
+}
