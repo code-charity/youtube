e8a4e7e5b7e6c1e5c84ebd4d53632777ad6068ad
Priyank Patel <priyank.mcp@gmail.com>
2020-07-18 10:49:06 +0530

    #314 Adding support dislike button color red
    Adding support of changing Dislike button color red

--- src/youtube/js/features/shortcuts.js
+++ src/youtube/js/features/shortcuts.js
@@ -242,6 +242,10 @@ ImprovedTube.shortcuts = function() {
 
                 if (like) {
                     like.click();
+                    // Find if the active disliked element after click
+                    var activeDislikedElmt = document.querySelectorAll('#menu #top-level-buttons ytd-toggle-button-renderer.style-default-active')[0];
+                    // If it is disliked then change element color to red
+                    like.style.color = activeDislikedElmt ? 'red' : '#909090';
                 }
             },
             shortcut_dark_theme: function() {
