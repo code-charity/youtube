6ddc6596e4c5adb8cef5cac434fa7ae26aec4e3f
Victor Savinov <katashiroku@gmail.com>
2020-07-29 01:21:24 +0300

    Fixed playback speed step for shortcuts

--- src/youtube/js/features/shortcuts.js
+++ src/youtube/js/features/shortcuts.js
@@ -195,7 +195,7 @@ ImprovedTube.shortcuts = function() {
                 var player = document.querySelector('#movie_player');
 
                 if (player && player.setPlaybackRate && player.getPlaybackRate) {
-                    player.setPlaybackRate(player.getPlaybackRate() + (Number(ImprovedTube.storage.shortcut_playback_step) || .05));
+                    player.setPlaybackRate(player.getPlaybackRate() + (Number(ImprovedTube.storage.shortcut_playback_speed_step) || .05));
                 }
 
                 showStatus(player, player.getPlaybackRate());
@@ -204,7 +204,7 @@ ImprovedTube.shortcuts = function() {
                 var player = document.querySelector('#movie_player');
 
                 if (player && player.setPlaybackRate && player.getPlaybackRate) {
-                    player.setPlaybackRate(player.getPlaybackRate() - (Number(ImprovedTube.storage.shortcut_playback_step) || .05));
+                    player.setPlaybackRate(player.getPlaybackRate() - (Number(ImprovedTube.storage.shortcut_playback_speed_step) || .05));
                 }
 
                 showStatus(player, player.getPlaybackRate());
