44b76eeb8fb5a260c1b72853e358ea506599129c
ImprovedTube <ImprovedTube@gmail.com>
2021-05-02 04:35:39 +0200

    Update firefox-addon-store-builder.py

--- firefox-addon-store-builder.py
+++ firefox-addon-store-builder.py
@@ -1,10 +1,10 @@
 ToDo / Required: 
-1. remove  dynamic innerHtml combined with "remote content" (googleapis.com/youtube)
-2. remove  google analytics (or add it only for google webstore version)
+1. removing  dynamic innerHtml combined with "remote content" (googleapis.com/youtube)
+2. removing  google analytics (or add it only for google webstore version)
 
 More:
-3. add m.youtube / *.youtube.com  (firefox add on is available on phones!) 
+3. adding m.youtube / *.youtube.com  (firefox add on is available on phones!) 
 (but repo should also have m.youtube  permissions(,facebook,everywhere)
-(only the Google Webstore version remains www.youtube, until we announce change there)
+(only the Google Webstore version remains www.youtube - unless we announce a change there)
 
-4. Firefox version currently has commented   // video.parentNode.parentNode.pauseVideo();   as autopause bug was confusing
+4. Firefox version currently has uncommented   // video.parentNode.parentNode.pauseVideo();   as autopause bug was confusing
