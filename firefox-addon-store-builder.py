ae2e1d21be9381492a4c8aa3930570c0a2972c1e
ImprovedTube <ImprovedTube@gmail.com>
2021-05-02 20:57:02 +0200

    Update firefox-addon-store-builder.py

--- firefox-addon-store-builder.py
+++ firefox-addon-store-builder.py
@@ -3,7 +3,7 @@ ToDo / Required:
 2. removing  google analytics (or add it only for google webstore version)
 
 More:
-3. adding m.youtube / *.youtube.com  (firefox add on is available on phones!) 
+3. adding m.youtube / *.youtube.com  (firefox addons are available on phones!) 
 (but repo should also have m.youtube  permissions(,facebook,everywhere)
 (only the Google Webstore version remains www.youtube - unless we announce a change there)
 
