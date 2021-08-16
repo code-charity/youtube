5143b3f1136e57be3948bf92b1cb23bbb962c23b
Victor Savinov <katashiroku@gmail.com>
2021-08-16 14:13:55 +0300

    Fixed "Subtitles window color" feature

--- youtube-scripts.js
+++ youtube-scripts.js
@@ -2012,9 +2012,10 @@ ImprovedTube.subtitlesWindowColor = function () {
     var option = this.storage.subtitles_window_color;
 
     if (this.isset(option)) {
-        var player = this.elements.player;
+        var player = this.elements.player,
+            button = this.elements.player_subtitles_button;
 
-        if (player && player.getSubtitlesUserSettings) {
+        if (player && player.getSubtitlesUserSettings && button && button.getAttribute('aria-pressed') === 'true') {
             var settings = player.getSubtitlesUserSettings();
 
             if (settings) {
