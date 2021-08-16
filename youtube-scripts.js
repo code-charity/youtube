533c4647b1154f66443268d3cff932ead9aa7977
Victor Savinov <katashiroku@gmail.com>
2021-08-16 14:15:46 +0300

    Fixed "Subtitles font opacity" feature

--- youtube-scripts.js
+++ youtube-scripts.js
@@ -2084,9 +2084,10 @@ ImprovedTube.subtitlesFontOpacity = function () {
     var option = this.storage.subtitles_font_opacity;
 
     if (this.isset(option)) {
-        var player = this.elements.player;
+        var player = this.elements.player,
+            button = this.elements.player_subtitles_button;
 
-        if (player && player.getSubtitlesUserSettings) {
+        if (player && player.getSubtitlesUserSettings && button && button.getAttribute('aria-pressed') === 'true') {
             var settings = player.getSubtitlesUserSettings();
 
             if (settings) {
