1b66351a804de2f3ac36dfddbb6a18ffe1e032a0
Victor Savinov <katashiroku@gmail.com>
2021-08-16 14:12:18 +0300

    Fixed "Subtitles background color" feature

--- youtube-scripts.js
+++ youtube-scripts.js
@@ -1943,7 +1943,7 @@ ImprovedTube.subtitlesFontSize = function () {
         var player = this.elements.player,
             button = this.elements.player_subtitles_button;
 
-        if (player && player.getSubtitlesUserSettings && button && button.getAttribute('aria-pressed') === 'true) {
+        if (player && player.getSubtitlesUserSettings && button && button.getAttribute('aria-pressed') === 'true') {
             var settings = player.getSubtitlesUserSettings();
 
             if (settings) {
@@ -1964,9 +1964,10 @@ ImprovedTube.subtitlesBackgroundColor = function () {
     var option = this.storage.subtitles_background_color;
 
     if (this.isset(option)) {
-        var player = this.elements.player;
+        var player = this.elements.player,
+            button = this.elements.player_subtitles_button;
 
-        if (player && player.getSubtitlesUserSettings) {
+        if (player && player.getSubtitlesUserSettings && button && button.getAttribute('aria-pressed') === 'true) {
             var settings = player.getSubtitlesUserSettings();
 
             if (settings) {
