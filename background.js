7573cdcf1ff96355178f2b0ce9d1ec6894707164
Victor Savinov <katashiroku@gmail.com>
2022-07-05 07:54:10 +0300

    The code has been formatted

--- background.js
+++ background.js
@@ -19,28 +19,28 @@
 var _gaq = [];
 
 function googleAnalytics(previous_time) {
-    var version = chrome.runtime.getManifest().version,
-        script = document.createElement('script'),
-        current_time = new Date().getTime();
+	var version = chrome.runtime.getManifest().version,
+		script = document.createElement('script'),
+		current_time = new Date().getTime();
 
-    _gaq.push(['_setAccount', 'UA-88354155-1']);
-    _gaq.push(['_setSessionCookieTimeout', 14400000]);
+	_gaq.push(['_setAccount', 'UA-88354155-1']);
+	_gaq.push(['_setSessionCookieTimeout', 14400000]);
 
-    if (current_time - (previous_time || 0) >= 86400000) {
-        _gaq.push([
-            '_trackPageview',
-            '/improvedtube-' + version + '/background',
-            'page-loaded'
-        ]);
+	if (current_time - (previous_time || 0) >= 86400000) {
+		_gaq.push([
+			'_trackPageview',
+			'/improvedtube-' + version + '/background',
+			'page-loaded'
+		]);
 
-        chrome.storage.local.set({
-            ga: current_time
-        });
-    }
+		chrome.storage.local.set({
+			ga: current_time
+		});
+	}
 
-    script.src = 'https://ssl.google-analytics.com/ga.js';
+	script.src = 'https://ssl.google-analytics.com/ga.js';
 
-    document.body.appendChild(script);
+	document.body.appendChild(script);
 }
 
 
@@ -49,7 +49,7 @@ function googleAnalytics(previous_time) {
 --------------------------------------------------------------*/
 
 function uninstallURL() {
-    chrome.runtime.setUninstallURL('https://improvedtube.com/uninstalled');
+	chrome.runtime.setUninstallURL('https://improvedtube.com/uninstalled');
 }
 
 
@@ -58,33 +58,33 @@ function uninstallURL() {
 --------------------------------------------------------------*/
 
 function updateContextMenu(locale) {
-    var items = [
-        'donate',
-        'rateMe',
-        'GitHub'
-    ];
+	var items = [
+		'donate',
+		'rateMe',
+		'GitHub'
+	];
 
-    chrome.contextMenus.removeAll();
+	chrome.contextMenus.removeAll();
 
-    for (var i = 0; i < 3; i++) {
-        var item = items[i];
+	for (var i = 0; i < 3; i++) {
+		var item = items[i];
 
-        chrome.contextMenus.create({
-            id: String(i),
-            title: locale[item] || item,
-            contexts: ['browser_action']
-        });
-    }
+		chrome.contextMenus.create({
+			id: String(i),
+			title: locale[item] || item,
+			contexts: ['browser_action']
+		});
+	}
 
-    chrome.contextMenus.onClicked.addListener(function (info) {
-        var links = [
-            'https://www.improvedtube.com/donate',
-            'https://chrome.google.com/webstore/detail/improve-youtube-video-you/bnomihfieiccainjcjblhegjgglakjdd',
-            'https://github.com/code4charity/YouTube-Extension'
-        ];
+	chrome.contextMenus.onClicked.addListener(function (info) {
+		var links = [
+			'https://www.improvedtube.com/donate',
+			'https://chrome.google.com/webstore/detail/improve-youtube-video-you/bnomihfieiccainjcjblhegjgglakjdd',
+			'https://github.com/code4charity/YouTube-Extension'
+		];
 
-        window.open(links[info.menuItemId]);
-    });
+		window.open(links[info.menuItemId]);
+	});
 }
 
 
@@ -93,61 +93,61 @@ function updateContextMenu(locale) {
 --------------------------------------------------------------*/
 
 chrome.tabs.onActivated.addListener(function (activeInfo) {
-    chrome.tabs.sendMessage(activeInfo.tabId, {
-        action: 'focus'
-    });
+	chrome.tabs.sendMessage(activeInfo.tabId, {
+		action: 'focus'
+	});
 
-    chrome.tabs.query({
-        windowId: activeInfo.windowId
-    }, function (tabs) {
-        if (tabs) {
-            for (var i = 0, l = tabs.length; i < l; i++) {
-                if (tabs[i].id !== activeInfo.tabId) {
-                    chrome.tabs.sendMessage(tabs[i].id, {
-                        action: 'blur'
-                    });
-                }
-            }
-        }
-    });
+	chrome.tabs.query({
+		windowId: activeInfo.windowId
+	}, function (tabs) {
+		if (tabs) {
+			for (var i = 0, l = tabs.length; i < l; i++) {
+				if (tabs[i].id !== activeInfo.tabId) {
+					chrome.tabs.sendMessage(tabs[i].id, {
+						action: 'blur'
+					});
+				}
+			}
+		}
+	});
 });
 
 chrome.windows.onFocusChanged.addListener(function (windowId) {
-    chrome.windows.getAll(function (windows) {
-        for (var i = 0, l = windows.length; i < l; i++) {
-            if (windows[i].focused === true) {
-                chrome.tabs.query({
-                    windowId: windows[i].id
-                }, function (tabs) {
-                    if (tabs) {
-                        for (var j = 0, k = tabs.length; j < k; j++) {
-                            var tab = tabs[j];
+	chrome.windows.getAll(function (windows) {
+		for (var i = 0, l = windows.length; i < l; i++) {
+			if (windows[i].focused === true) {
+				chrome.tabs.query({
+					windowId: windows[i].id
+				}, function (tabs) {
+					if (tabs) {
+						for (var j = 0, k = tabs.length; j < k; j++) {
+							var tab = tabs[j];
 
-                            if (tab.active) {
-                                chrome.tabs.sendMessage(tab.id, {
-                                    action: 'focus'
-                                });
-                            }
-                        }
-                    }
-                });
-            } else {
-                chrome.tabs.query({
-                    windowId: windows[i].id
-                }, function (tabs) {
-                    if (tabs) {
-                        for (var j = 0, k = tabs.length; j < k; j++) {
-                            var tab = tabs[j];
+							if (tab.active) {
+								chrome.tabs.sendMessage(tab.id, {
+									action: 'focus'
+								});
+							}
+						}
+					}
+				});
+			} else {
+				chrome.tabs.query({
+					windowId: windows[i].id
+				}, function (tabs) {
+					if (tabs) {
+						for (var j = 0, k = tabs.length; j < k; j++) {
+							var tab = tabs[j];
 
-                            chrome.tabs.sendMessage(tab.id, {
-                                action: 'blur'
-                            });
-                        }
-                    }
-                });
-            }
-        }
-    });
+							chrome.tabs.sendMessage(tab.id, {
+								action: 'blur'
+							});
+						}
+					}
+				});
+			}
+		}
+	});
 });
 
 
@@ -156,95 +156,95 @@ chrome.windows.onFocusChanged.addListener(function (windowId) {
 --------------------------------------------------------------*/
 
 function migration(items) {
-    if (items.hd_thumbnails === true) {
-        items.thumbnails_quality = 'maxresdefault';
-    }
+	if (items.hd_thumbnails === true) {
+		items.thumbnails_quality = 'maxresdefault';
+	}
 
-    for (var key in items) {
-        var item = items[key];
+	for (var key in items) {
+		var item = items[key];
 
-        if (key.indexOf('shortcut') !== -1 && typeof item === 'string') {
-            try {
-                item = JSON.parse(item);
+		if (key.indexOf('shortcut') !== -1 && typeof item === 'string') {
+			try {
+				item = JSON.parse(item);
 
-                var value = {
-                    alt: item.altKey,
-                    ctrl: item.ctrlKey,
-                    shift: item.shiftKey
-                };
+				var value = {
+					alt: item.altKey,
+					ctrl: item.ctrlKey,
+					shift: item.shiftKey
+				};
 
-                if (item.hasOwnProperty('key') && item.hasOwnProperty('keyCode')) {
-                    value.keys = {};
+				if (item.hasOwnProperty('key') && item.hasOwnProperty('keyCode')) {
+					value.keys = {};
 
-                    value.keys[item.keyCode] = {
-                        key: item.key
-                    };
-                }
+					value.keys[item.keyCode] = {
+						key: item.key
+					};
+				}
 
-                if (item.hasOwnProperty('wheel')) {
-                    value.wheel = item.wheel < 0 ? -1 : 1;
-                }
+				if (item.hasOwnProperty('wheel')) {
+					value.wheel = item.wheel < 0 ? -1 : 1;
+				}
 
-                items[key] = value;
-            } catch (error) {
-                console.log(error);
-            }
-        }
-    }
+				items[key] = value;
+			} catch (error) {
+				console.log(error);
+			}
+		}
+	}
 
-    if (items.theme_my_colors === true) {
-        items.theme = 'my-colors';
-    } else if (items.default_dark_theme === true) {
-        items.theme = 'dark';
-    } else if (items.night_theme === true) {
-        items.theme = 'night';
-    } else if (items.dawn_theme === true) {
-        items.theme = 'dawn';
-    } else if (items.sunset_theme === true) {
-        items.theme = 'sunset';
-    } else if (items.desert_theme === true) {
-        items.theme = 'desert';
-    } else if (items.plain_theme === true) {
-        items.theme = 'plain';
-    } else if (items.black_theme === true) {
-        items.theme = 'black';
-    }
+	if (items.theme_my_colors === true) {
+		items.theme = 'my-colors';
+	} else if (items.default_dark_theme === true) {
+		items.theme = 'dark';
+	} else if (items.night_theme === true) {
+		items.theme = 'night';
+	} else if (items.dawn_theme === true) {
+		items.theme = 'dawn';
+	} else if (items.sunset_theme === true) {
+		items.theme = 'sunset';
+	} else if (items.desert_theme === true) {
+		items.theme = 'desert';
+	} else if (items.plain_theme === true) {
+		items.theme = 'plain';
+	} else if (items.black_theme === true) {
+		items.theme = 'black';
+	}
 
-    if (typeof items.theme_primary_color === 'string') {
-        var match = items.theme_primary_color.match(/[0-9.]+/g);
+	if (typeof items.theme_primary_color === 'string') {
+		var match = items.theme_primary_color.match(/[0-9.]+/g);
 
-        if (match) {
-            for (var i = 0, l = match.length; i < l; i++) {
-                match[i] = parseFloat(match[i]);
-            }
-        }
+		if (match) {
+			for (var i = 0, l = match.length; i < l; i++) {
+				match[i] = parseFloat(match[i]);
+			}
+		}
 
-        items.theme_primary_color = match;
-    }
+		items.theme_primary_color = match;
+	}
 
-    if (typeof items.theme_text_color === 'string') {
-        var match = items.theme_text_color.match(/[0-9.]+/g);
+	if (typeof items.theme_text_color === 'string') {
+		var match = items.theme_text_color.match(/[0-9.]+/g);
 
-        if (match) {
-            for (var i = 0, l = match.length; i < l; i++) {
-                match[i] = parseFloat(match[i]);
-            }
-        }
+		if (match) {
+			for (var i = 0, l = match.length; i < l; i++) {
+				match[i] = parseFloat(match[i]);
+			}
+		}
 
-        items.theme_text_color = match;
-    }
+		items.theme_text_color = match;
+	}
 
-    chrome.storage.local.set(items);
+	chrome.storage.local.set(items);
 
-    chrome.storage.local.remove('hd_thumbnails');
-    chrome.storage.local.remove('theme_my_colors');
-    chrome.storage.local.remove('default_dark_theme');
-    chrome.storage.local.remove('night_theme');
-    chrome.storage.local.remove('dawn_theme');
-    chrome.storage.local.remove('sunset_theme');
-    chrome.storage.local.remove('desert_theme');
-    chrome.storage.local.remove('plain_theme');
-    chrome.storage.local.remove('black_theme');
+	chrome.storage.local.remove('hd_thumbnails');
+	chrome.storage.local.remove('theme_my_colors');
+	chrome.storage.local.remove('default_dark_theme');
+	chrome.storage.local.remove('night_theme');
+	chrome.storage.local.remove('dawn_theme');
+	chrome.storage.local.remove('sunset_theme');
+	chrome.storage.local.remove('desert_theme');
+	chrome.storage.local.remove('plain_theme');
+	chrome.storage.local.remove('black_theme');
 }
 
 
@@ -257,9 +257,9 @@ function migration(items) {
 --------------------------------------------------------------*/
 
 chrome.storage.local.get(function (items) {
-    uninstallURL();
-    //googleAnalytics(items.ga);
-    migration(items);
+	uninstallURL();
+	//googleAnalytics(items.ga);
+	migration(items);
 });
 
 
@@ -268,78 +268,78 @@ chrome.storage.local.get(function (items) {
 --------------------------------------------------------------*/
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    var name = request.name;
+	var name = request.name;
 
-    if (name === 'migration') {
-        chrome.storage.local.get(function (items) {
-            try {
-                migration(items);
-            } catch (error) {}
+	if (name === 'migration') {
+		chrome.storage.local.get(function (items) {
+			try {
+				migration(items);
+			} catch (error) {}
 
-            setTimeout(function () {
-                if (sender.tab) {
-                    sendResponse(sender.tab.id);
-                }
-            }, 500);
-        });
+			setTimeout(function () {
+				if (sender.tab) {
+					sendResponse(sender.tab.id);
+				}
+			}, 500);
+		});
 
-        return true;
-    } else if (name === 'only-one-player') {
-        chrome.tabs.query({}, function (tabs) {
-            for (var i = 0, l = tabs.length; i < l; i++) {
-                var tab = tabs[i];
+		return true;
+	} else if (name === 'only-one-player') {
+		chrome.tabs.query({}, function (tabs) {
+			for (var i = 0, l = tabs.length; i < l; i++) {
+				var tab = tabs[i];
 
-                if (sender.tab.id !== tab.id) {
-                    chrome.tabs.sendMessage(tab.id, {
-                        action: 'pause'
-                    });
-                }
-            }
-        });
-    } else if (name === 'download') {
-        chrome.permissions.request({
-            permissions: ['downloads'],
-            origins: ['https://www.youtube.com/*']
-        }, function (granted) {
-            if (granted) {
-                try {
-                    var blob = new Blob([JSON.stringify(request.value)], {
-                        type: 'application/json;charset=utf-8'
-                    });
+				if (sender.tab.id !== tab.id) {
+					chrome.tabs.sendMessage(tab.id, {
+						action: 'pause'
+					});
+				}
+			}
+		});
+	} else if (name === 'download') {
+		chrome.permissions.request({
+			permissions: ['downloads'],
+			origins: ['https://www.youtube.com/*']
+		}, function (granted) {
+			if (granted) {
+				try {
+					var blob = new Blob([JSON.stringify(request.value)], {
+						type: 'application/json;charset=utf-8'
+					});
 
-                    chrome.downloads.download({
-                        url: URL.createObjectURL(blob),
-                        filename: request.filename,
-                        saveAs: true
-                    });
-                } catch (error) {
-                    console.error(error);
-                }
-            } else {
-                console.error('Permission is not granted.');
-            }
-        });
-    } else if (name === 'export') {
-        chrome.storage.local.get(function (data) {
-            chrome.permissions.request({
-                permissions: ['downloads'],
-                origins: ['https://www.youtube.com/*']
-            }, function (granted) {
-                if (granted) {
-                    var blob = new Blob([JSON.stringify(data)], {
-                            type: 'application/octet-stream'
-                        }),
-                        date = new Date();
+					chrome.downloads.download({
+						url: URL.createObjectURL(blob),
+						filename: request.filename,
+						saveAs: true
+					});
+				} catch (error) {
+					console.error(error);
+				}
+			} else {
+				console.error('Permission is not granted.');
+			}
+		});
+	} else if (name === 'export') {
+		chrome.storage.local.get(function (data) {
+			chrome.permissions.request({
+				permissions: ['downloads'],
+				origins: ['https://www.youtube.com/*']
+			}, function (granted) {
+				if (granted) {
+					var blob = new Blob([JSON.stringify(data)], {
+							type: 'application/octet-stream'
+						}),
+						date = new Date();
 
-                    chrome.downloads.download({
-                        url: URL.createObjectURL(blob),
-                        filename: 'improvedtube-' + (date.getMonth() + 1) + '-' + date.getDate() + '-' + date.getFullYear() + '.txt',
-                        saveAs: true
-                    });
-                }
-            });
-        });
-    }
+					chrome.downloads.download({
+						url: URL.createObjectURL(blob),
+						filename: 'improvedtube-' + (date.getMonth() + 1) + '-' + date.getDate() + '-' + date.getFullYear() + '.txt',
+						saveAs: true
+					});
+				}
+			});
+		});
+	}
 });
 
 
@@ -348,7 +348,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 --------------------------------------------------------------*/
 
 chrome.runtime.onInstalled.addListener(function (details) {
-    chrome.storage.local.get(function (items) {
-        migration(items);
-    });
+	chrome.storage.local.get(function (items) {
+		migration(items);
+	});
 });
--- ui/page.js
+++ ui/page.js
@@ -3,30 +3,30 @@
 --------------------------------------------------------------*/
 
 document.addEventListener('DOMContentLoaded', function () {
-    var charset = document.createElement('meta'),
-        viewport = document.createElement('meta'),
-        title = document.createElement('title'),
-        style = document.createElement('style');
+	var charset = document.createElement('meta'),
+		viewport = document.createElement('meta'),
+		title = document.createElement('title'),
+		style = document.createElement('style');
 
-    charset.setAttribute('charset', 'utf-8');
-    viewport.setAttribute('name', 'viewport');
-    viewport.setAttribute('content', 'width=device-width,initial-scale=1');
+	charset.setAttribute('charset', 'utf-8');
+	viewport.setAttribute('name', 'viewport');
+	viewport.setAttribute('content', 'width=device-width,initial-scale=1');
 
-    title.textContent = 'ImprovedTube';
-    style.textContent = 'body{width:100vw;height:100vh}';
+	title.textContent = 'ImprovedTube';
+	style.textContent = 'body{width:100vw;height:100vh}';
 
-    for (var i = document.head.childNodes.length - 1; i > -1; i--) {
-        document.head.childNodes[i].remove();
-    }
+	for (var i = document.head.childNodes.length - 1; i > -1; i--) {
+		document.head.childNodes[i].remove();
+	}
 
-    document.head.appendChild(charset);
-    document.head.appendChild(viewport);
-    document.head.appendChild(title);
-    document.head.appendChild(style);
+	document.head.appendChild(charset);
+	document.head.appendChild(viewport);
+	document.head.appendChild(title);
+	document.head.appendChild(style);
 
-    document.documentElement.setAttribute('page', true);
+	document.documentElement.setAttribute('page', true);
 
-    for (var i = document.body.childNodes.length - 1; i > -1; i--) {
-        document.body.childNodes[i].remove();
-    }
+	for (var i = document.body.childNodes.length - 1; i > -1; i--) {
+		document.body.childNodes[i].remove();
+	}
 });
