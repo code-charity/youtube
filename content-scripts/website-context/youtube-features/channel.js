f4f54b09bf4b93867ed7a92babf2bf3d7bac3459
Alan Tan <dodieboy@users.noreply.github.com>
2022-10-23 22:10:38 +0800

    Update channelDefaultTab()

--- content-scripts/website-context/youtube-features/channel.js
+++ content-scripts/website-context/youtube-features/channel.js
@@ -11,11 +11,7 @@ ImprovedTube.channelDefaultTab = function (a) {
 
 	if (option && option !== '/' && a.parentNode && a.parentNode.id !== 'contenteditable-root') {
 		if (this.regex.channel_home_page.test(a.href) && !a.href.endsWith(option)) {
-			if (!a.dataset.itOrigin) {
-				a.dataset.itOrigin = a.href.replace(this.regex.channel_home_page_postfix, '');
-			}
-
-			a.href = a.dataset.itOrigin + option;
+			a.href = a.href.replace(this.regex.channel_home_page_postfix, '') + option;
 
 			a.addEventListener('click', function (event) {
 				event.stopPropagation();
