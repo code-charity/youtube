3e91eaeebb813f511c8154ab945fb33a824cb557
ImprovedTube <ImprovedTube@gmail.com>
2023-03-19 22:54:16 +0100

    comments on the side

--- content-scripts/website-context/youtube-features/appearance.js
+++ content-scripts/website-context/youtube-features/appearance.js
@@ -154,11 +154,13 @@ ImprovedTube.playerRemainingDuration = function () {
 ------------------------------------------------------------------------------*/
 ImprovedTube.commentsSidebarPosition=()=>{ if(ImprovedTube.storage.comments_sidebar_position){ 
       document.querySelector("#columns").appendChild(document.querySelector("#comments"));
-// }else{document.querySelector("#columns").appendChild(document.querySelector("#secondary"));
-  if(window.matchMedia("(max-width: 1438px)").matches){ document.querySelector("#primary-inner").appendChild(document.querySelector("#secondary"));}
+  
+  if(window.matchMedia("(max-width: 1598px)").matches)
+    {document.querySelector("#primary-inner").appendChild(document.querySelector("#secondary"));}
 		// console.log("secondary and its children have been appended to primary-inner")	
-//   }else{ document.querySelector("#below").appendChild(document.querySelector("#comments"));
-   }   
+
+} else{document.querySelector("#below").appendChild(document.querySelector("#comments")); 
+	   document.querySelector("#columns").appendChild(document.querySelector("#secondary"));}
 }
 /*------------------------------------------------------------------------------
 4.2.2 SIDEBAR
