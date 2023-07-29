023721080f973ac644d5653878044f3efdfda3cc
Alan Tan <dodieboy@users.noreply.github.com>
2023-07-30 01:34:55 +0800

    Optimize childHandler
    added yt-icon-shape

--- js&css/web-accessible/functions.js
+++ js&css/web-accessible/functions.js
@@ -3,7 +3,8 @@
 --------------------------------------------------------------*/
 
 ImprovedTube.childHandler = function (node) {
-	if (node.nodeName === 'SCRIPT' || node.nodeName === 'iron-iconset-svg' || node.nodeName === 'svg' || node.nodeName === '#text' || node.nodeName === '#comment' || node.nodeName === 'SPAN' || node.nodeName === 'DOM-IF' || node.nodeName === 'DOM-REPEAT') {
+	//console.log(node.nodeName);
+	if (node.nodeName === 'SCRIPT' || node.nodeName === 'iron-iconset-svg' || node.nodeName === 'svg' || node.nodeName === 'SPAN' || node.nodeName === '#text' || node.nodeName === '#comment' || node.nodeName === 'yt-icon-shape' || node.nodeName === 'DOM-IF' || node.nodeName === 'DOM-REPEAT') {
 		return
 	}
 	var children = node.children;
