449fbdc36771a538ab8c0dc0d806a588bea40ab9
MAZ <29178226+MAZ01001@users.noreply.github.com>
2023-11-15 05:25:00 +0100

    added calls to playlistPopupUpdate

--- js&css/web-accessible/functions.js
+++ js&css/web-accessible/functions.js
@@ -97,6 +97,7 @@ ImprovedTube.ytElementsHandler = function (node) {
 				this.playlistReverse();
 			}
 		}
+		this.playlistPopupUpdate();
 	} else if (name === 'YTD-GUIDE-SECTION-RENDERER') {
 		if (!this.elements.sidebar_section) {
 			this.elements.sidebar_section = node;
@@ -122,7 +123,9 @@ ImprovedTube.ytElementsHandler = function (node) {
 		if(document.documentElement.dataset.pageType === 'video'){
             this.hideDetailButton(node.$['flexible-item-buttons'].children);
         }
-	} else if (name === 'YTD-SUBSCRIBE-BUTTON-RENDERER') {
+	} else if (name === 'YTD-PLAYLIST-HEADER-RENDERER' || (name === 'YTD-MENU-RENDERER' && node.classList.contains('ytd-playlist-panel-renderer'))) {
+		this.playlistPopupUpdate();
+ 	} else if (name === 'YTD-SUBSCRIBE-BUTTON-RENDERER') {
 		if (node.className.indexOf('ytd-c4-tabbed-header-renderer') !== -1) {
 			ImprovedTube.blacklist('channel', node);
 		}
@@ -255,8 +258,6 @@ ImprovedTube.ytElementsHandler = function (node) {
 			ImprovedTube.chapters(node);}, 200);
 		}
 	}
-	// TODO move do different ifs ↑ to minimize calls (~ mini player, video player, and playlist page)
-	this.playlistPopup();
 
 };
 
