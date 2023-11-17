56ff48672c45c29ef928e150e1409c9d6cc0e3ba
ImprovedTube <ImprovedTube@gmail.com>
2023-11-17 04:25:26 +0100

    Popup Player: no url bar

--- js&css/extension/init.js
+++ js&css/extension/init.js
@@ -147,6 +147,14 @@ document.addEventListener('it-message-from-youtube', function () {
 			chrome.runtime.sendMessage({
 				name: 'only-one-player'
 			});
+		} else if (message.action === 'popup player') {
+			chrome.runtime.sendMessage({   
+			action: 'fixPopup',    
+			url: message.url, 
+			playerSize: { width: message.width, height: message.height   },
+			title: message.title
+			});
+			
 		} else if (message.action === 'analyzer') {
 			if (extension.storage.data.analyzer_activation === true) {
 				var data = message.name,
