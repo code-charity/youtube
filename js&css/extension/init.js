e4dfc2568eb969f5a8fdb9b5e0fd7da442efc8cf
Asifur Rahaman <asifurrahaman754@gmail.com>
2023-11-22 20:33:46 +0600

    Add hide video title on fullscreen feature

--- js&css/extension/init.js
+++ js&css/extension/init.js
@@ -44,6 +44,7 @@ extension.events.on('init', function () {
 	extension.features.relatedVideos();
 	extension.features.comments();
 	extension.features.openNewTab();
+	extension.features.hideVideoTitleFullScreen();
 	bodyReady();
 });
 
