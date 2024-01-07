a503f54d6080c569754d4f731db23017b62fdf37
ImprovedTube <ImprovedTube@gmail.com>
2024-01-07 07:18:01 +0100

    Update player.js

--- js&css/web-accessible/www.youtube.com/player.js
+++ js&css/web-accessible/www.youtube.com/player.js
@@ -67,10 +67,10 @@ FORCED PLAYBACK SPEED
 ImprovedTube.playerPlaybackSpeed = function () { if (this.storage.player_forced_playback_speed === true) {
 		var player = this.elements.player,
 		video = player.querySelector('video'),
-		if ( video.playbackRate < 1  ||  video.playbackRate > 1 ) { return; }
 		option = this.storage.player_playback_speed;
 		if (this.isset(option) === false) { option = 1; }
-	    if ( !player.getVideoData().isLive || player.getVideoData().isLive === false)
+		else if ( option !== 1 && (video.playbackRate < 1  ||  video.playbackRate > 1)) { return; }
+		if ( !player.getVideoData().isLive || player.getVideoData().isLive === false)
 { player.setPlaybackRate(Number(option));	video.playbackRate = Number(option);  // #1729 q2	// hi! @raszpl
 	if  ( (this.storage.player_force_speed_on_music !== true  || this.storage.player_dont_speed_education === true) 
 	     	&& option !== 1) {
