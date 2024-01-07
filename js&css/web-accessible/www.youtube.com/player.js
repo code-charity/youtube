d94dbcdc3def064d3df40a0d65ce8d1a890dd418
ImprovedTube <ImprovedTube@gmail.com>
2024-01-07 07:06:45 +0100

    testing if speed is already other than 1x before considering to set automatically set speed.

--- js&css/web-accessible/www.youtube.com/player.js
+++ js&css/web-accessible/www.youtube.com/player.js
@@ -64,13 +64,13 @@ ImprovedTube.playerAutoPip = function () {
 /*------------------------------------------------------------------------------
 FORCED PLAYBACK SPEED
 ------------------------------------------------------------------------------*/
-ImprovedTube.playerPlaybackSpeed = function () {
-	if (this.storage.player_forced_playback_speed === true) {
+ImprovedTube.playerPlaybackSpeed = function () { if (this.storage.player_forced_playback_speed === true) {
 		var player = this.elements.player,
 		video = player.querySelector('video'),
+		if ( video.playbackRate < 1  ||  video.playbackRate > 1 ) { return; }
 		option = this.storage.player_playback_speed;
-		if (this.isset(option) === false) {	option = 1; }			
-	if ( !player.getVideoData().isLive || player.getVideoData().isLive === false)
+		if (this.isset(option) === false) { option = 1; }
+	    if ( !player.getVideoData().isLive || player.getVideoData().isLive === false)
 { player.setPlaybackRate(Number(option));	video.playbackRate = Number(option);  // #1729 q2	// hi! @raszpl
 	if  ( (this.storage.player_force_speed_on_music !== true  || this.storage.player_dont_speed_education === true) 
 	     	&& option !== 1) {
@@ -182,9 +182,9 @@ function getRandomInvidiousInstance() { return invidiousInstances[Math.floor(Mat
 							else { ImprovedTube.fetchDOMData();} }  
 })();
 }
-   }    // else { }  
-  }
- }        
+    }    // else { }  
+  }        
+ }
 } 
 /*------------------------------------------------------------------------------
 SUBTITLES
