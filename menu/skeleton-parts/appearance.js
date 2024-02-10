4fa591fa2ac36b9fecbe338a043b49474a41b10d
ImprovedTube <ImprovedTube@gmail.com>
2024-02-10 01:51:00 +0100

    Update appearance.js

--- menu/skeleton-parts/appearance.js
+++ menu/skeleton-parts/appearance.js
@@ -725,10 +725,6 @@ extension.skeleton.main.layers.section.appearance.on.click.comments = {
 				component: "switch",
 				text: "hideCommentsCount",
 				tags: "hide,remove"
-			},
-			columns: {
-				component: "switch",
-				text: "toggleColumnComments"
 			}
 		}
 	}
