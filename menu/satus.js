code-charity/youtube

commit 220d429107a9fa3f11633a4529d83dce7f2d0e86

Update satus.js component: 'slider' is storage: false, respect that

menu/satus.js
@@ -1895,7 +1895,7 @@ satus.components.colorPicker = function(component, skeleton) {
 							var modal = this.skeleton.parentSkeleton.parentSkeleton,
 								hsl = modal.value;
 
-							hsl[0] = this.storage.value;
+							hsl[0] = this.value;
 
 							this.previousSibling.style.backgroundColor = 'hsl(' + hsl[0] + 'deg,' + hsl[1] + '%, ' + hsl[2] + '%)';
 							this.parentNode.previousSibling.style.backgroundColor = 'hsl(' + hsl[0] + 'deg, 100%, 50%)';
@@ -2010,13 +2010,12 @@ satus.components.slider = function(component, skeleton) {
 	input.min = skeleton.min || 0;
 	input.max = skeleton.max || 1;
 	input.step = skeleton.step || 1;
-	input.value = component.storage.value || skeleton.value || 0;
+	input.value = component.storage?.value || skeleton.value || 0;
 
 	text_input.addEventListener('blur', function() {
 		var component = this.parentNode.parentNode;
 
 		component.input.value = Number(this.value.replace(/[^0-9.]/g, ''));
-		component.storage.value = Number(component.input.value);
 
 		component.update();
 	});
@@ -2026,7 +2025,6 @@ satus.components.slider = function(component, skeleton) {
 			var component = this.parentNode.parentNode;
 
 			component.input.value = Number(this.value.replace(/[^0-9.]/g, ''));
-			component.storage.value = Number(component.input.value);
 
 			component.update();
 		}
@@ -2035,7 +2033,7 @@ satus.components.slider = function(component, skeleton) {
 	input.addEventListener('input', function() {
 		var component = this.parentNode.parentNode;
 
-		component.storage.value = Number(this.value);
+		component.value = Number(this.value);
 
 		component.update();
 	});

commit 13b4494f8661e923e80a4b73512a16197a92a097

Update satus.js make sliders remove default value
I broke half the sliders with https://github.com/code-charity/youtube/commit/220d429107a9fa3f11633a4529d83dce7f2d0e86, fixed now

menu/satus.js
@@ -2012,20 +2012,12 @@ satus.components.radio = function(component, skeleton) {
 /*--------------------------------------------------------------
 >>> SLIDER
 --------------------------------------------------------------*/
-
 satus.components.slider = function(component, skeleton) {
-	var content = component.createChildElement('div', 'content'),
+	const content = component.createChildElement('div', 'content'),
 		children_container = content.createChildElement('div', 'children-container'),
 		text_input = content.createChildElement('input'),
 		track_container = component.createChildElement('div', 'track-container'),
 		input = track_container.createChildElement('input', 'input');
-
-	component.childrenContainer = children_container;
-	component.textInput = text_input;
-	component.input = input;
-	component.track = track_container.createChildElement('div', 'track');
-
-	text_input.type = 'text';
 
 	input.type = 'range';
 	input.min = skeleton.min || 0;
@@ -2033,8 +2025,16 @@ satus.components.slider = function(component, skeleton) {
 	input.step = skeleton.step || 1;
 	input.value = component.storage?.value || skeleton.value || 0;
 
+	component.childrenContainer = children_container;
+	component.input = input;
+	component.track = track_container.createChildElement('div', 'track');
+	component.track.style.width = 100 / (input.max - input.min) * (input.value - input.min) + '%';
+	component.textInput = text_input;
+	component.textInput.type = 'text';
+	component.textInput.value = input.value;
+
 	text_input.addEventListener('blur', function() {
-		var component = this.parentNode.parentNode;
+		const component = this.parentNode.parentNode;
 
 		component.input.value = Number(this.value.replace(/[^0-9.]/g, ''));
 
@@ -2043,7 +2043,7 @@ satus.components.slider = function(component, skeleton) {
 
 	text_input.addEventListener('keydown', function(event) {
 		if (event.key === 'Enter') {
-			var component = this.parentNode.parentNode;
+			const component = this.parentNode.parentNode;
 
 			component.input.value = Number(this.value.replace(/[^0-9.]/g, ''));
 
@@ -2052,7 +2052,7 @@ satus.components.slider = function(component, skeleton) {
 	});
 
 	input.addEventListener('input', function() {
-		var component = this.parentNode.parentNode;
+		const component = this.parentNode.parentNode;
 
 		component.value = Number(this.value);
 
@@ -2060,17 +2060,22 @@ satus.components.slider = function(component, skeleton) {
 	});
 
 	component.update = function() {
-		var input = this.input;
+		const input = this.input;
 
 		this.textInput.value = input.value;
+		if (component.storage) {
+			if (component.skeleton.value == Number(input.value)) {
+				component.storage.value = 'satus_remove';
+			} else {
+				component.storage.value = Number(input.value);
+			}
+		}
 
 		this.track.style.width = 100 / (input.max - input.min) * (input.value - input.min) + '%';
 	};
 
-	component.update();
-
 	if (skeleton.on) {
-		for (var type in skeleton.on) {
+		for (const type in skeleton.on) {
 			input.addEventListener(type, function(event) {
 				this.parentNode.parentNode.dispatchEvent(new Event(event.type));
 			});