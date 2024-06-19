ca8483379c4ae8567ebfbc6e8efdb80af87a45c2
Rasz_pl <citizenr@gmail.com>
2024-06-20 01:22:35 +0200

    Update blocklist.js safety

--- js&css/web-accessible/www.youtube.com/blocklist.js
+++ js&css/web-accessible/www.youtube.com/blocklist.js
@@ -8,192 +8,218 @@
 
 ImprovedTube.blocklist = function (type, node) {
 	if (this.storage.blocklist_activate) {
-		if (type === 'video') {
-			if (node.nodeName !== 'A' || !node.href) { alert(1) };
-			const video = node.href.match(ImprovedTube.regex.video_id)?.[1],
-				  channel = node.parentNode.__dataHost?.__data?.data?.shortBylineText?.runs?.[0]?.navigationEndpoint?.commandMetadata?.webCommandMetadata?.url ? node.parentNode.__dataHost.__data.data.shortBylineText.runs[0].navigationEndpoint.commandMetadata.webCommandMetadata.url.match(ImprovedTube.regex.channel).groups.name : undefined;
-			let mode = 'video',
-				blockedElement;
-			if (!video) return; // no video ID, something went horribly wrong, bail
-
-			// YT reuses VIDEO elements dynamically, need to monitor and also dynamically readjust BLOCK style
-			if (!this.elements.observerList.includes(node)) {
-				// YT reuses VIDEO elements dynamically, need to monitor and also dynamically readjust BLOCK style whenever href is modified
-				this.blocklistObserver.observe(node, {attributes: true,
-													  attributeFilter: ['href']});
-				// keep track to only attach one observer per element
-				this.elements.observerList.push(node);
-			}
-
-			switch(node.parentNode.className.replace('style-scope ','')) {
-				case 'ytd-compact-video-renderer':
-					// list next to player
-					// node.parentNode.__dataHost.$.dismissible;
-				case 'ytd-rich-item-renderer':
-					// short reel
-				case 'ytd-rich-grid-media':
-					// grid reel
-				case 'ytd-rich-grid-slim-media':
-					// short grid reel
-				case 'ytd-playlist-video-renderer':
-					// playlist page
-				case 'ytd-playlist-panel-video-renderer':
-					// playlist next to player
-					// node.parentNode.closest('ytd-playlist-panel-video-renderer')
-				case 'ytd-structured-description-video-lockup-renderer':
-					// list under the player
-					// node.parentNode.closest('ytd-structured-description-video-lockup-renderer')
-					// or even node.parentNode.closest('ytd-compact-infocard-renderer') === node.parentNode.parentNode.parentNode.parentNode
-					blockedElement = node.parentNode.parentNode.parentNode;
-					break;
-				case 'ytd-grid-video-renderer':
-					// channel home screen grid
-				case 'ytd-reel-item-renderer':
-					// reel
-					blockedElement = node.parentNode.parentNode;
-					break;
-			}
-
-			if (!blockedElement) return; // couldnt find valid enveloping element, bail
-
-			node.blockedElement = blockedElement;
-	if (this.storage.blocklist) {	
-		if (this.storage.blocklist.videos) {
-			if (this.storage.blocklist.videos[video] && !blockedElement.classList.contains('it-blocklisted-video')) {
-				// blocklisted video
-				blockedElement.classList.add('it-blocklisted-video');
-			} else if (!this.storage.blocklist.videos[video] && blockedElement.classList.contains('it-blocklisted-video')) {
-				// video not blocklisted, show it
-				blockedElement.classList.remove('it-blocklisted-video');
-			}
-		} 	
-			if (channel && this.storage.blocklist.channels ) {
-				// this thumbnail has channel information, can try channel blocklist
-				if (this.storage.blocklist.channels[channel] && !blockedElement.classList.contains('it-blocklisted-channel')) {
-					// blocked channel? = block all videos from that channel
-					blockedElement.classList.add('it-blocklisted-channel');
-				} else if (!this.storage.blocklist.channels[channel] && blockedElement.classList.contains('it-blocklisted-channel')) {
-					// channel not blocked, show it
-					blockedElement.classList.remove('it-blocklisted-channel');
+		
+		switch(type) {
+			case 'video':
+				const video = node?.href?.match(ImprovedTube.regex.video_id)?.[1] || (node?.classList?.contains('ytd-video-preview')?'video-preview':undefined),
+					channel = node?.parentNode?.__dataHost?.__data?.data?.shortBylineText?.runs?.[0]?.navigationEndpoint?.commandMetadata?.webCommandMetadata?.url?.match(ImprovedTube.regex.channel)?.groups?.name;
+				let blockedElement;
+	
+				if (!video) return; // no video ID, something went horribly wrong, bail
+	
+				// YT reuses VIDEO elements dynamically, need to monitor and also dynamically readjust BLOCK style
+				if (!this.elements.observerList.includes(node)) {
+					// YT reuses VIDEO elements dynamically, need to monitor and also dynamically readjust BLOCK style whenever href is modified
+					this.blocklistObserver.observe(node, {attributes: true,
+														attributeFilter: ['href']});
+					// keep track to only attach one observer per element
+					this.elements.observerList.push(node);
 				}
-			}
-	}
-			if (node.querySelector("button.it-add-to-blocklist")) return; // skip blocklist button creation if one already exists
+	
+				switch(node.parentNode.className.replace('style-scope ','')) {
+					case 'ytd-compact-video-renderer':
+						// list next to player
+						// node.parentNode.__dataHost.$.dismissible;
+					case 'ytd-rich-item-renderer':
+						// short reel
+					case 'ytd-rich-grid-media':
+						// grid reel
+					case 'ytd-rich-grid-slim-media':
+						// short grid reel
+					case 'ytd-playlist-video-renderer':
+						// playlist page
+					case 'ytd-playlist-panel-video-renderer':
+						// playlist next to player
+						// node.parentNode.closest('ytd-playlist-panel-video-renderer')
+					case 'ytd-structured-description-video-lockup-renderer':
+						// list under the player
+						// node.parentNode.closest('ytd-structured-description-video-lockup-renderer')
+						// or even node.parentNode.closest('ytd-compact-infocard-renderer') === node.parentNode.parentNode.parentNode.parentNode
+					case 'ytd-video-renderer':
+						// search results
+					case 'ytd-video-preview':
+						// subscriptions/search thumbnail video-preview
+						blockedElement = node.parentNode.parentNode.parentNode;
+						break;
+	
+					case 'ytd-grid-video-renderer':
+						// channel home screen grid
+					case 'ytd-reel-item-renderer':
+						// reel
+						blockedElement = node.parentNode.parentNode;
+						break;
+	
+					default:
+						// unknown, bail out
+						return;
+						break;
+				}
+	
+				node.blockedElement = blockedElement;
+	
+				if (this.storage.blocklist) {
+					if (this.storage.blocklist.videos && ImprovedTube.storage.blocklist.videos[video]) {
+						// blocklisted video
+						blockedElement.classList.add('it-blocklisted-video');
+					} else {
+						// video not blocklisted, show it
+						blockedElement.classList.remove('it-blocklisted-video');
+					}
+					if (this.storage.blocklist.channels && channel && ImprovedTube.storage.blocklist.channels[channel]) {
+						// blocked channel
+						blockedElement.classList.add('it-blocklisted-channel');
+					} else {
+						// channel not blocked, show it
+						blockedElement.classList.remove('it-blocklisted-channel');
+					}
+				}
+	
+				if (node.querySelector("button.it-add-to-blocklist")) return; // skip blocklist button creation if one already exists
+	
+				let buttonV = document.createElement('button'),
+					svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg'),
+					path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+	
+				buttonV.className = 'it-add-to-blocklist';
+				buttonV.addEventListener('click', function (event) {
+					event.preventDefault();
+					event.stopPropagation();
+					if (this.parentNode.href) {
+						const video = this.parentNode.href?.match(ImprovedTube.regex.video_id)?.[1],
+							channel = this.parentNode.parentNode?.__dataHost?.__data?.data?.shortBylineText?.runs?.[0]?.navigationEndpoint?.commandMetadata?.webCommandMetadata?.url?.match(ImprovedTube.regex.channel)?.groups?.name;
+							
+							// Yes, this is horrible. Cant find better way of extracting title :(
+							title = this.parentNode?.__dataHost?.__data?.data?.title?.runs?.[0]?.text
+								|| this.parentNode.__dataHost?.__data?.data?.title?.simpleText
+								|| this.parentNode.__dataHost?.__data?.videoPreviewData?.accessibilityText,
+							blockedElement = node.blockedElement;
+						let added = false,
+							type = 'video';
+	
+						if (!video || !blockedElement || !title) {
+							console.error('blocklist: need video ID, blockedElement and title');	
+							return; 
+						}
 
-			let button = document.createElement('button'),
-				svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg'),
-				path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+						if (channel && blockedElement.classList.contains('it-blocklisted-channel')) {
+							// unblocking channel
+							type = 'channel';
+						} else if (blockedElement.classList.contains('it-blocklisted-video')) {
+							// unblocking blocklisted video
+						} else {
+							// nothing blocked, clicking should block this video
+							added = true;
+						}
+						ImprovedTube.messages.send({action: 'blocklist',
+													added: added,
+													type: type,
+													id: type == 'channel' ? channel : video,
+													title: title});
+					}
+				}, true);
+	
+				svg.setAttributeNS(null, 'viewBox', '0 0 24 24');
+				path.setAttributeNS(null, 'd', 'M12 2a10 10 0 100 20 10 10 0 000-20zm0 18A8 8 0 015.69 7.1L16.9 18.31A7.9 7.9 0 0112 20zm6.31-3.1L7.1 5.69A8 8 0 0118.31 16.9z');
+	
+				svg.appendChild(path);
+				buttonV.appendChild(svg);
+	
+				node.appendChild(buttonV);
+				this.elements.blocklist_buttons.push(buttonV);
+				break;
 
-			button.className = 'it-add-to-blocklist';
-			button.addEventListener('click', function (event) {
-				if (this.parentNode.href) {
+			case 'channel':
+				let buttonC = node?.parentNode?.parentNode?.querySelector("button.it-add-channel-to-blocklist"),
+					id = location.href.match(ImprovedTube.regex.channel)?.groups?.name;
 
-					const video = node.href.match(ImprovedTube.regex.video_id)?.[1],
-						  channel = node.parentNode.__dataHost?.__data?.data?.shortBylineText?.runs?.[0]?.navigationEndpoint?.commandMetadata?.webCommandMetadata?.url ? node.parentNode.__dataHost.__data.data.shortBylineText.runs[0].navigationEndpoint.commandMetadata.webCommandMetadata.url.match(ImprovedTube.regex.channel).groups.name : undefined,
-						  data = this.parentNode.__dataHost.__data?.data?.title,
-						  blockedElement = node.blockedElement;
-					let title,
-						added = false,
-						type = 'video';
+				if (!id) {
+					console.error('blocklist: no channel ID');	
+					return; 
+				}
 
-					if (!video || !blockedElement) return; // need both video ID and blockedElement, otherwise bail
+				// skip channel blocklist button creation if one already exists
+				if (buttonC) {
+					if (this.storage.blocklist.channels[id]) {
+						buttonC.innerText = 'Remove from blocklist';
+					} else if (!this.storage.blocklist.channels[id]) {
+						buttonC.innerText = 'Add to blocklist';
+					}
+					return;
+				}
+	
+				buttonC = document.createElement('button');
+				buttonC.className = 'it-add-channel-to-blocklist';
+	
+				if (this.storage.blocklist.channels[id]) {
+					buttonC.innerText = 'Remove from blocklist';
+				} else {
+					buttonC.innerText = 'Add to blocklist';
+				}
+	
+				buttonC.addEventListener('click', function (event) {
+					const data = ytInitialData?.metadata?.channelMetadataRenderer,
+					//let data = this.parentNode.__dataHost.__data.data,
+						id = location.href.match(ImprovedTube.regex.channel)?.groups?.name;
+					let added = false;
 
-					if (data?.runs?.[0]?.text) {
-						title = data.runs[0].text;
-					} else if (data?.simpleText) {
-						title = data.simpleText;
+					event.preventDefault();
+					event.stopPropagation();
+					
+					if (!id || !data) {
+						console.error('blocklist click: no channel ID or metadata');	
+						return; 
 					}
 
-					if (channel && blockedElement.classList.contains('it-blocklisted-channel')) {
-						// unblocking channel
-						type = 'channel';
-					} else if (blockedElement.classList.contains('it-blocklisted-video')) {
-						// unblocking blocklisted video
+					if (ImprovedTube.storage.blocklist.channels[id]) {
+						delete ImprovedTube.storage.blocklist.channels[id];
+						buttonC.innerText = 'Add to blocklist';
 					} else {
-						// nothing blocked, clicking should block this video
+						ImprovedTube.storage.blocklist.channels[id] = {title: data.title,
+																	preview: data.avatar?.thumbnails?.[0]?.url};
+						buttonC.innerText = 'Remove from blocklist';
 						added = true;
 					}
 					ImprovedTube.messages.send({action: 'blocklist',
 												added: added,
-												type: type,
-												id: type == 'channel' ? channel : video,
-												title: title});
-					event.preventDefault();
-					event.stopPropagation();
+												type: 'channel',
+												id: id,
+												title: data.title,
+												preview: data.avatar?.thumbnails[0].url});
+				}, true);
+	
+				node.parentNode.parentNode.appendChild(buttonC);
+				this.elements.blocklist_buttons.push(buttonC);
+				break;
+
+			default:
+				// initialize and scan whole page. Called only once on load after 'storage-loaded'.
+				if (!this.storage.blocklist) {
+					this.storage.blocklist = {videos: {}, channels: {}};
 				}
-			}, true);
-
-			svg.setAttributeNS(null, 'viewBox', '0 0 24 24');
-			path.setAttributeNS(null, 'd', 'M12 2a10 10 0 100 20 10 10 0 000-20zm0 18A8 8 0 015.69 7.1L16.9 18.31A7.9 7.9 0 0112 20zm6.31-3.1L7.1 5.69A8 8 0 0118.31 16.9z');
-
-			svg.appendChild(path);
-			button.appendChild(svg);
-
-			node.appendChild(button);
-			this.elements.blocklist_buttons.push(button);
-		} else if (type === 'channel') {
-			let button = node.parentNode.parentNode.querySelector("button.it-add-channel-to-blocklist"),
-				id = location.href.match(ImprovedTube.regex.channel).groups.name;
-
-			// skip channel blocklist button creation if one already exists
-			if (button) {
-				if (this.storage.blocklist.channels[id] && button.added) {
-					button.innerText = 'Remove from blocklist';
-					button.added = false;
-				} else if (!this.storage.blocklist.channels[id] && !button.added) {
-					button.innerText = 'Add to blocklist';
-					button.added = true;
+				if (!this.storage.blocklist.videos) {
+					this.storage.blocklist.videos = {};
 				}
-				return;
-			}
-
-			button = document.createElement('button');
-			button.className = 'it-add-channel-to-blocklist';
-
-			if (this.storage.blocklist.channels[id]) {
-				button.innerText = 'Remove from blocklist';
-				button.added = false;
-			} else {
-				button.innerText = 'Add to blocklist';
-				button.added = true;
-			}
-
-			button.addEventListener('click', function (event) {
-				const data = ytInitialData.metadata.channelMetadataRenderer,
-				//let data = this.parentNode.__dataHost.__data.data,
-					id = location.href.match(ImprovedTube.regex.channel).groups.name;
-
-				if (this.added) { // adding
-					ImprovedTube.storage.blocklist.channels[id] = {title: data.title,
-																   preview: data.avatar.thumbnails[0].url};
-					button.innerText = 'Remove from blocklist';
-				} else { // removing
-					delete ImprovedTube.storage.blocklist.channels[id];
-					button.innerText = 'Add to blocklist';
+				if (!this.storage.blocklist.channels) {
+					this.storage.blocklist.channels = {};
 				}
-				ImprovedTube.messages.send({action: 'blocklist',
-											added: this.added,
-											type: 'channel',
-											id: id,
-											title: data.title,
-											preview: data.avatar.thumbnails[0].url});
-				this.added = !this.added;
-
-				event.preventDefault();
-				event.stopPropagation();
-			}, true);
-
-			node.parentNode.parentNode.appendChild(button);
-			this.elements.blocklist_buttons.push(button);
-		} else if (arguments.length == 0) {
-			// scan whole page
-			for (let thumbnails of document.querySelectorAll('a.ytd-thumbnail[href]')) {
-				this.blocklist('video', thumbnails);
+				for (let thumbnails of document.querySelectorAll('a.ytd-thumbnail[href], a.ytd-video-preview')) {
+					this.blocklist('video', thumbnails);
+				}
+				if (document.querySelector('YT-SUBSCRIBE-BUTTON-VIEW-MODEL')) {
+					this.blocklist('channel', document.querySelector('YT-SUBSCRIBE-BUTTON-VIEW-MODEL'));
+				}
+				break;
 			}
-			if (document.querySelector('YT-SUBSCRIBE-BUTTON-VIEW-MODEL')) {
-				this.blocklist('channel', document.querySelector('YT-SUBSCRIBE-BUTTON-VIEW-MODEL'));
-			}
-		}
 	} else {
 		// remove blocklist buttons
 		for (let blocked of this.elements.blocklist_buttons) {
@@ -207,39 +233,41 @@ ImprovedTube.blocklist = function (type, node) {
 			ImprovedTube.blocklistObserver.disconnect();
 		}
 		// remove all blocks from videos\channels
-		for (let blocked of document.querySelectorAll('.it-blocklisted-video, .it-blocklisted-channel')) {
+		for (let blocked of document.querySelectorAll('.it-blocklisted-video')) {
 			blocked.classList.remove('it-blocklisted-video');
+		}
+		for (let blocked of document.querySelectorAll('.it-blocklisted-channel')) {
 			blocked.classList.remove('it-blocklisted-channel');
 		}
 	}
 };
 
 ImprovedTube.blocklistObserver = new MutationObserver(function (mutationList) {
-	for (var mutation of mutationList) {
-		const video = mutation.target.href.match(ImprovedTube.regex.video_id)?.[1],
-			  channel = mutation.target.parentNode.__dataHost?.__data?.data?.shortBylineText?.runs?.[0]?.navigationEndpoint?.commandMetadata?.webCommandMetadata?.url ? mutation.target.parentNode.__dataHost.__data.data.shortBylineText.runs[0].navigationEndpoint.commandMetadata.webCommandMetadata.url.match(ImprovedTube.regex.channel).groups.name : undefined,
+	for (const mutation of mutationList) {
+		const video = mutation.target.href?.match(ImprovedTube.regex.video_id)?.[1],
+			  channel = mutation.target.parentNode?.__dataHost?.__data?.data?.shortBylineText?.runs?.[0]?.navigationEndpoint?.commandMetadata?.webCommandMetadata?.url?.match(ImprovedTube.regex.channel)?.groups?.name,
 			  blockedElement = mutation.target.blockedElement;
 
-		if (!video || !blockedElement) return; // need both video ID and blockedElement, otherwise bail
+		if (!blockedElement) return; // missing blockedElement? lets panic and run away!
+		
+		if (!video) {
+			// nop video ID means most likely video-preview node went inactive
+			blockedElement.classList.remove('it-blocklisted-video');
+			blockedElement.classList.remove('it-blocklisted-channel');
+			return;
+		}
 
-		if (ImprovedTube.storage.blocklist.videos[video]) {
-			if (!blockedElement.classList.contains('it-blocklisted-video')) {
+		if (ImprovedTube.storage.blocklist) {
+			if (ImprovedTube.storage.blocklist.videos && ImprovedTube.storage.blocklist.videos[video]) {
 				blockedElement.classList.add('it-blocklisted-video');
-			}
-		} else {
-			if (blockedElement.classList.contains('it-blocklisted-video')) {
+			} else {
 				blockedElement.classList.remove('it-blocklisted-video');
 			}
+			if (ImprovedTube.storage.blocklist.channels && channel && ImprovedTube.storage.blocklist.channels[channel]) {
+				blockedElement.classList.add('it-blocklisted-channel');
+			} else {
+				blockedElement.classList.remove('it-blocklisted-channel');
+			}
 		}
-
-
-		if (channel && ImprovedTube.storage.blocklist.channels[channel] && !blockedElement.classList.contains('it-blocklisted-channel')) {
-			// blocked channel? = block all videos from that channel
-			blockedElement.classList.add('it-blocklisted-channel');
-		} else if ((!channel || !ImprovedTube.storage.blocklist.channels[channel]) && blockedElement.classList.contains('it-blocklisted-channel')) {
-			// channel not blocked, show it
-			blockedElement.classList.remove('it-blocklisted-channel');
-		}
-
 	}
 });
