9b61bbd19337e9891435f31c6216e6c62e5d7e65
ImprovedTube <ImprovedTube@gmail.com>
2024-07-18 09:30:22 +0200

    csslint attempt; files in folders #2345 #2438 #2446 @PTheocharis @dodieboy @raszpl

--- tests/eslint_rules.config.mjs
+++ tests/eslint_rules.config.mjs
@@ -1,3 +1,28 @@
+const rules = {
+	indent: ["error", "tab", {"SwitchCase": 1}],
+	"brace-style": ["error", "1tbs", { "allowSingleLine": true }],
+	"no-empty": ["error", { "allowEmptyCatch": true }],
+	"no-unused-vars": ["error", { "caughtErrors": "none" }],
+	"no-undef": "off",
+	"no-trailing-spaces": "warn",
+	"no-multi-spaces": "warn",
+	"semi-spacing": "warn",
+	"comma-spacing": "warn",
+	"no-fallthrough": ["error", { "allowEmptyCase": true }],
+	"no-implicit-globals": "error",
+	"no-multiple-empty-lines": ["error", {"max": 1, "maxBOF": 0, "maxEOF": 0}],
+	"keyword-spacing": ["warn", { "before": true, "after": true }],
+	"space-before-function-paren": "warn",
+	"space-before-blocks": "warn",
+	"max-len": ["error", {
+	  code: 255,
+	  ignoreUrls: true,
+	  ignoreStrings: true,
+	  ignoreComments: true,
+	  ignoreRegExpLiterals: true
+	}]
+  };
+
 import globals from "globals";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
@@ -35,28 +60,5 @@ export default [...compat.extends("eslint:recommended"), ...compatConfiguration,
 			}
 		}
 	},
-	rules: {
-		indent: ["error", "tab", {"SwitchCase": 1}],
-		"brace-style": ["error", "1tbs", { "allowSingleLine": true }],
-		"no-empty": ["error", { "allowEmptyCatch": true }],
-		"no-unused-vars": ["error", { "caughtErrors": "none" }],
-		"no-undef": ["off"],
-		"no-trailing-spaces": "warn",
-		"no-multi-spaces": "warn",
-		"semi-spacing": "warn",
-		"comma-spacing": "warn",
-		"no-fallthrough": ["error", { "allowEmptyCase": true }],
-		"no-implicit-globals": "error",
-		"no-multiple-empty-lines": ["error", {"max": 1, "maxBOF": 0, "maxEOF": 0}],
-		"keyword-spacing": ["warn", { "before": true, "after": true }],
-		"space-before-function-paren": "warn",
-		"space-before-blocks": "warn",
-		"max-len": ["error", {
-			code: 255,
-			ignoreUrls: true,
-			ignoreStrings: true,
-			ignoreComments: true,
-			ignoreRegExpLiterals: true
-		}]
-	}
+	rules: rules
 }];
