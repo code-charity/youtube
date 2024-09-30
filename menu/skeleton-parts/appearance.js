2a2ea083c3852147c12e955a55030cc040c321f6
ImprovedTube <ImprovedTube@gmail.com>
2024-09-30 22:45:34 +0200

    Update appearance.js

--- menu/skeleton-parts/appearance.js
+++ menu/skeleton-parts/appearance.js
@@ -72,6 +72,9 @@ extension.skeleton.main.layers.section.appearance.on.click.header = {
 					text: "normal",
 					value: "normal"
 				}, {
+					text: "hover",
+					value: "hover"
+				}, {
 					text: "hoverOnVideoPage",
 					value: "hover_on_video_page"
 				}, {
@@ -83,9 +86,6 @@ extension.skeleton.main.layers.section.appearance.on.click.header = {
 				}, {
 					text: "hidden",
 					value: "hidden"
-				}, {
-					text: "hover",
-					value: "hover"
 				}],
 				tags: "hide,hover,static,top"
 			},
