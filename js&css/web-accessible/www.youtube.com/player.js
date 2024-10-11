d4fff385b7be9adced2ac5c66140410934f45f02
ImprovedTube <ImprovedTube@gmail.com>
2024-10-11 10:03:17 +0200

    Update player.js

--- js&css/web-accessible/www.youtube.com/player.js
+++ js&css/web-accessible/www.youtube.com/player.js
@@ -127,17 +127,20 @@ ImprovedTube.playbackSpeed = function (newSpeed) {
 	return newSpeed;
 };
 /*------------------------------------------------------------------------------
-FORCED PLAYBACK SPEED
+PERMANENT PLAYBACK SPEED
 ------------------------------------------------------------------------------*/
 ImprovedTube.playerPlaybackSpeed = function () { if (this.storage.player_forced_playback_speed === true) {
-	var player = this.elements.player,
-		video = player.querySelector('video'),
-		option = this.storage.player_playback_speed;
+	var player = this.elements.player; if (!player) return;
+	var video = this.elements.video || player.querySelector('video'); 
+	option = this.storage.player_playback_speed;	
 	if (this.isset(option) === false) { option = 1; }
-	else if ( option !== 1 && video.playbackRate !== option && (video.playbackRate > 1 || video.playbackRate < 1) )
+	else if ( option !== 1 ) { 
+		const speed = video?.playbackRate ? Number(video.playbackRate.toFixed(2)) : (player?.getPlaybackRate ? Number(player.getPlaybackRate().toFixed(2)) : null);
+		 if ( speed !== option && (speed > 1 || speed < 1) )
 		   { console.log("skipping permanent speed, since speed was manually set differently for this video to:" + video.playbackRate); return; }
-	if ( !player.getVideoData().isLive || player.getVideoData().isLive === false)
-	{ player.setPlaybackRate(Number(option));	video.playbackRate = Number(option); // #1729 q2	// hi! @raszpl
+	}
+	if (!(player.getVideoData() && player.getVideoData().isLive))
+	{ player.setPlaybackRate(Number(option)); if (!video) { video = { playbackRate: 1 }; };	video.playbackRate = Number(option); // #1729 q2	// hi! @raszpl
 		if ( (this.storage.player_force_speed_on_music !== true || this.storage.player_dont_speed_education === true)
 		 	&& option !== 1) {
 			ImprovedTube.speedException = function () {
