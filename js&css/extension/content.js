d8088f9440010c2f8c2cb16e48eedf1c1f2b471a
Anh Chien Vu <vuanhchien003@gmail.com>
2024-12-05 16:56:39 -0500

    Use SVG as icon for All Videos and More Details Link. Adjust the css for the Details area. Modularize the checkSwitchStateAndFetchData() function to DRY code

--- js&css/extension/content.js
+++ js&css/extension/content.js
@@ -9,85 +9,100 @@ function getVideoIdFromUrl() {
 function sendVideoIdToBackground() {
     const videoId = getVideoIdFromUrl();
     if (videoId) {
-        console.log('Sending video ID to background:', videoId); 
+        //console.log('Sending video ID to background:', videoId); 
         chrome.runtime.sendMessage({ action: 'store-video-id', videoId: videoId }, function(response) {
-            console.log('Response from background:', response); 
+            //console.log('Response from background:', response); 
         });
     }
 }
 
 function createChannelInfo(channelName, uploadTime, videoCount, customUrl) {
-    console.log('Creating channel info:', channelName, uploadTime, videoCount, customUrl); // Debugging log
+    //console.log('Creating channel info:', channelName, uploadTime, videoCount, customUrl); // Debugging log
 	const container = document.createElement('div');
 	container.className = 'channel-info';
 
-    // Add a line break after the upload time
-    const lineBreak = document.createElement('br');
+    const channelInfoContainer = document.createElement('div');
 
 	if (uploadTime) {
 		const uploadTimeElement = document.createElement('span');
 		uploadTimeElement.className = 'upload-time';
 		uploadTimeElement.textContent = `${uploadTime}`;
-		container.appendChild(uploadTimeElement);
-        container.appendChild(lineBreak);
+        channelInfoContainer.appendChild(uploadTimeElement);
+		container.appendChild(channelInfoContainer);
 	}
-
-	const allVideosLink = document.createElement('a');
-	allVideosLink.className = 'all-videos-link';
-	allVideosLink.href = `https://www.youtube.com/${customUrl}/videos`;
-	allVideosLink.textContent = 'All videos';
-	container.appendChild(allVideosLink);
-
-	if (videoCount) {
+    if (videoCount) {
 		const videoCountElement = document.createElement('span');
 		videoCountElement.className = 'video-count';
 		videoCountElement.textContent = ` (${videoCount} videos)`;
-		container.appendChild(videoCountElement);
-        container.appendChild(lineBreak);
+        channelInfoContainer.appendChild(videoCountElement);
+		container.appendChild(channelInfoContainer);
 	}
 
-	const viewDataLink = document.createElement('a');
-	viewDataLink.className = 'view-data-link';
-	viewDataLink.href = 'https://ytlarge.com/youtube/video-data-viewer/';
-	viewDataLink.textContent = 'View video data';
-	container.appendChild(viewDataLink);
+    const allVideosLink = document.createElement('a');
+    allVideosLink.className = 'all-videos-link';
+    allVideosLink.href = `https://www.youtube.com/${customUrl}/videos`;
+    allVideosLink.title = 'All Videos';
+
+    // Inline SVG
+    allVideosLink.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px" fill="gray">
+        <path d="M4 4h6v6H4V4zm0 10h6v6H4v-6zm10-10h6v6h-6V4zm0 10h6v6h-6v-6z"/>
+    </svg>
+    `;
+    container.appendChild(allVideosLink);
+
+    const viewDataLink = document.createElement('a');
+    viewDataLink.className = 'view-data-link';
+    viewDataLink.href = 'https://ytlarge.com/youtube/video-data-viewer/';
+    // Tooltip text using `title`
+    viewDataLink.title = 'View detailed video data';
+
+    // Information SVG Icon
+    viewDataLink.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px" fill="gray">
+        <path d="M11 7h2V5h-2v2zm1 14c-5.52 0-10-4.48-10-10S6.48 1 12 1s10 4.48 10 10-4.48 10-10 10zm-1-5h2v-6h-2v6z"/>
+    </svg>
+    `;
+    container.appendChild(viewDataLink);
 
     // Add custom CSS styles
     const style = document.createElement('style');
     style.textContent = `
         .channel-info {
-            background-color: #f9f9f9;
-            border: 1px solid #ddd;
-            padding: 10px;
-            margin-top: 10px;
-            margin-left: 10px;
-            margin-right: 10px;
-            border-radius: 5px;
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            gap: 2px;
+            align-items: center;
+            margin-right: 2px;
         }
         .channel-info .channel-name {
             font-weight: bold;
             display: block;
             margin-bottom: 5px;
         }
-        .channel-info .upload-time {
+        .channel-info .upload-time, .channel-info .video-count {
             color: #555;
             display: block;
             margin-bottom: 5px;
+            width: 80px;
+            font-size: 13px;
+            font-weight: bold;
         }
         .channel-info .all-videos-link,
         .channel-info .view-data-link {
-            color: #1a73e8;
-            text-decoration: none;
-            margin-right: 10px;
+            font-family: 'Roboto', Arial, sans-serif;
+            font-size: 13px;
+            font-weight: bold;
+            border: none;
+            border-radius: 20px;
+            cursor: pointer;
+            padding: 8px 8px;
+            transition: background-color 0.3s ease, transform 0.2s ease;
         }
         .channel-info .all-videos-link:hover,
         .channel-info .view-data-link:hover {
             text-decoration: underline;
-        }
-        .channel-info .video-count {
-            color: #333;
-            display: block;
-            margin-top: 5px;
         }
     `;
     document.head.appendChild(style);
@@ -95,8 +110,25 @@ function createChannelInfo(channelName, uploadTime, videoCount, customUrl) {
 	return container;
 }
 
+// Check if the switch is on and fetch new data. Otherwise, remove the details block
+function checkSwitchStateAndFetchData() {
+    chrome.runtime.sendMessage({ action: 'check-switch-state' }, function(response) {
+        if (response.isSwitchOn) {
+            chrome.runtime.sendMessage({ action: 'fetch-new-data' });
+        } else {
+            // Remove existing video details if the switch is off
+            const targetElement = document.querySelector('.channel-info')
+            if (targetElement) {
+                targetElement.remove();
+            }
+        }
+    });
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     sendVideoIdToBackground()
+    //console.log('Content script loaded');
+
     let previousVideoId = getVideoIdFromUrl();
 
     // Listen for changes in the video URL and send the updated video ID
@@ -104,26 +136,25 @@ document.addEventListener('DOMContentLoaded', function() {
         const currentVideoId = getVideoIdFromUrl();
         if (currentVideoId !== previousVideoId) {
             previousVideoId = currentVideoId;
+
             sendVideoIdToBackground();
 
-            // Check if the switch is on and fetch new-data
-            chrome.runtime.sendMessage({ action: 'fetch-new-data' });
+            checkSwitchStateAndFetchData();
         }
-    })
+    });
     observer.observe(document.body, { childList: true, subtree: true });
 
     // Listen for visibility changes to handle page navigation
     document.addEventListener('visibilitychange', function() {
         if (document.visibilityState === 'visible') {
-            // Check if the switch is on and re-fetch data if the user returns to the video page
-            chrome.runtime.sendMessage({ action: 'fetch-new-data' });
+            checkSwitchStateAndFetchData();
         }
     });
 
     chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-        console.log('Received message from background:', message); // Debugging log
+        //console.log('Received message from background:', message); // Debugging log
         if (message.action === 'append-channel-info') {
-            console.log('Switch on Details mode'); 
+            //console.log('Switch on Details mode'); 
             const {channelName, uploadTime, videoCount, customUrl} = message;
             const channelInfo = createChannelInfo(channelName, uploadTime, videoCount, customUrl);
             const targetElement = document.querySelector('ytd-video-owner-renderer.style-scope.ytd-watch-metadata');
@@ -138,9 +169,9 @@ document.addEventListener('DOMContentLoaded', function() {
                 console.error('Target element not found');
             }
         } else if (message.action === 'remove-channel-info') {
-            console.log('Switch off Details mode');
+            //console.log('Switch off Details mode');
             const targetElement = document.querySelector('.channel-info');
-            console.log(targetElement)
+
             if(targetElement) {
                 targetElement.remove();
             } else {
