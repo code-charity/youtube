e79c55756900ff1843521724a49aa35510ec2c16
Shashank4516 <Shashankmalik23156@gmail.com>
2025-06-29 03:47:19 +0530

    Fix: Auto-pause YouTube video on tab switch using visibilitychange

--- js&css/extension/functions.js
+++ js&css/extension/functions.js
@@ -4,6 +4,19 @@
 # GET URL PARAMETER
 --------------------------------------------------------------*/
 extension.functions.getUrlParameter = function (url, parameter) {
-	var match = url.match(new RegExp('(\\?|\\&)' + parameter + '=[^&]+'));
-	if (match) {return match[0].substr(3);}
+  var match = url.match(new RegExp("(\\?|\\&)" + parameter + "=[^&]+"));
+  if (match) {
+    return match[0].substr(3);
+  }
 };
+
+document.addEventListener("visibilitychange", () => {
+  const videos = document.getElementsByTagName("video");
+  if (document.hidden && videos.length > 0) {
+    const video = videos[0];
+    if (!video.paused) {
+      video.pause();
+      console.log("[YT+]: Video paused (getElementsByTagName fallback)");
+    }
+  }
+});
