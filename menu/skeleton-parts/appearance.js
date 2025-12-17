6a229bb7a99a2c87c832385044ba5783c32f48fe
Alan Tan <dodieboy@users.noreply.github.com>
2025-12-17 20:29:00 +0800

    Add always show shorts control button
    Force control button to show on smaller screen (when max-height: 723px, max-width: 552px)

--- menu/skeleton-parts/appearance.js
+++ menu/skeleton-parts/appearance.js
@@ -134,305 +134,325 @@ extension.skeleton.main.layers.section.appearance.on.click.player = {
 	text: "player",
 	on: {
 		click: {
-			component: "section",
-			variant: "card",
-			forced_theater_mode: {
-				component: "switch",
-				text: "forcedTheaterMode",
-				tags: "wide",
-			},
-			player_size: {
-				component: "select",
-				variant: "player-size",
-				text: "playerSize",
-				options: [{
-					text: "doNotChange",
-					value: "do_not_change"
-				}, {
-					text: "fullWindow",
-					value: "full_window"
-				}, {
-					text: "Max. width within the page",
-					value: "max_width"
-				}, {
-					text: "fitToWindow",
-					value: "fit_to_window"
-				}, {
-					text: "240p",
-					value: "240p"
-				}, {
-					text: "360p",
-					value: "360p"
-				}, {
-					text: "480p",
-					value: "480p"
-				}, {
-					text: "576p",
-					value: "576p"
-				}, {
-					text: "720p",
-					value: "720p"
-				}, {
-					text: "1080p",
-					value: "1080p"
-				}, {
-					text: "1440p",
-					value: "1440p"
-				}, {
-					text: "2160p",
-					value: "2160p"
-				}, {
-					text: "custom",
-					value: "custom"
-				}]
-			},
-			customPlayerSizeSection: {
+			section_1: {
 				component: "section",
-				variant: "custom-player-size",
-
-				custom_player_size_width: {
-					component: "text-field",
-					placeholder: "1280",
-					rows: 1,
-					lineNumbers: false
+				variant: "card",
+				forced_theater_mode: {
+					component: "switch",
+					text: "forcedTheaterMode",
+					tags: "wide",
 				},
-				x: {
-					component: "span",
-					text: "x"
+				player_size: {
+					component: "select",
+					variant: "player-size",
+					text: "playerSize",
+					options: [{
+						text: "doNotChange",
+						value: "do_not_change"
+					}, {
+						text: "fullWindow",
+						value: "full_window"
+					}, {
+						text: "Max. width within the page",
+						value: "max_width"
+					}, {
+						text: "fitToWindow",
+						value: "fit_to_window"
+					}, {
+						text: "240p",
+						value: "240p"
+					}, {
+						text: "360p",
+						value: "360p"
+					}, {
+						text: "480p",
+						value: "480p"
+					}, {
+						text: "576p",
+						value: "576p"
+					}, {
+						text: "720p",
+						value: "720p"
+					}, {
+						text: "1080p",
+						value: "1080p"
+					}, {
+						text: "1440p",
+						value: "1440p"
+					}, {
+						text: "2160p",
+						value: "2160p"
+					}, {
+						text: "custom",
+						value: "custom"
+					}]
 				},
-				custom_player_size_height: {
-					component: "text-field",
-					placeholder: "720",
-					rows: 1,
-					lineNumbers: false
-				}
-			},
-			player_hide_annotations: {
-				component: "switch",
-				text: "hideAnnotations",
-				tags: "hide,remove,elements"
-			},
-			player_hide_cards: {
-				component: "switch",
-				text: "hideCards",
-				tags: "hide,remove,elements"
-			},
-			player_show_cards_on_mouse_hover: {
-				component: "switch",
-				text: "showCardsOnMouseHover",
-				tags: "hide,remove,elements"
-			},
-			player_hide_endscreen: {
-				component: "switch",
-				text: "hideEndscreen"
-			},
-			hide_includes_paid_promotion:{
-			component: 'switch',
-			text: 'hideIncludesPaidPromotion',
-			storage: 'hide_includes_paid_promotion',
-			},
-			remove_black_bars: {
-				component: "switch",
-				text: "removeBlackBars",
-				tags: "bars",
-			},
-			player_hide_controls: {
-				component: "select",
-				text: "hidePlayerControlsBar",
-				options: [{
-					text: "off",
-					value: "off",
-					default: "true"
-				}, {
-					text: "whenPaused",
-					value: "when_paused"
-				}, {
-					text: "always",
-					value: "always"
-				}]
-			},	
-			player_hide_progress_preview: {
-			component: 'switch',
-			text: 'hideProgressBarPreview',
-			storage: 'player_hide_progress_preview',
-			},			
-			player_hide_controls_options: {
-				component: "button",
-				text: "hidePlayerControlsBarButtons",
-				on: {
-					click: {
-						component: "section",
-						variant: "card",
+				customPlayerSizeSection: {
+					component: "section",
+					variant: "custom-player-size",
 
-						player_play_button: {
-							component: "switch",
-							text: "playPause"
-						},
-						player_previous_button: {
-							component: "switch",
-							text: "previousVideo"
-						},
-						player_next_button: {
-							component: "switch",
-							text: "nextVideo"
-						},
-						player_volume_button: {
-							component: "switch",
-							text: "volume"
-						},
-						player_autoplay_button: {
-							component: "switch",
-							text: "autoplay"
-						},
-						player_settings_button: {
-							component: "switch",
-							text: "settings"
-						},
-						player_subtitles_button: {
-							component: "switch",
-							text: "subtitles"
-						},
-						player_subtitlesLine_button: {
-							component: "switch",
-							text: "subtitleLine"
-						},
-						player_miniplayer_button: {
-							component: "switch",
-							text: "nativeMiniPlayer"
-						},
-						player_view_button: {
-							component: "switch",
-							text: "viewMode"
-						},
-						player_screen_button: {
-							component: "switch",
-							text: "screen"
-						},
-						player_remote_button: {
-							component: "switch",
-							text: "remote"
-						},
-						player_chaptertitle_button: {
-							component:"switch",
-							text:"chapterTitle"
+					custom_player_size_width: {
+						component: "text-field",
+						placeholder: "1280",
+						rows: 1,
+						lineNumbers: false
+					},
+					x: {
+						component: "span",
+						text: "x"
+					},
+					custom_player_size_height: {
+						component: "text-field",
+						placeholder: "720",
+						rows: 1,
+						lineNumbers: false
+					}
+				},
+				player_hide_annotations: {
+					component: "switch",
+					text: "hideAnnotations",
+					tags: "hide,remove,elements"
+				},
+				player_hide_cards: {
+					component: "switch",
+					text: "hideCards",
+					tags: "hide,remove,elements"
+				},
+				player_show_cards_on_mouse_hover: {
+					component: "switch",
+					text: "showCardsOnMouseHover",
+					tags: "hide,remove,elements"
+				},
+				player_hide_endscreen: {
+					component: "switch",
+					text: "hideEndscreen"
+				},
+				hide_includes_paid_promotion: {
+					component: 'switch',
+					text: 'hideIncludesPaidPromotion',
+					storage: 'hide_includes_paid_promotion',
+				},
+				remove_black_bars: {
+					component: "switch",
+					text: "removeBlackBars",
+					tags: "bars",
+				},
+				player_hide_controls: {
+					component: "select",
+					text: "hidePlayerControlsBar",
+					options: [{
+						text: "off",
+						value: "off",
+						default: "true"
+					}, {
+						text: "whenPaused",
+						value: "when_paused"
+					}, {
+						text: "always",
+						value: "always"
+					}]
+				},
+				player_hide_progress_preview: {
+					component: 'switch',
+					text: 'hideProgressBarPreview',
+					storage: 'player_hide_progress_preview',
+				},
+				player_hide_controls_options: {
+					component: "button",
+					text: "hidePlayerControlsBarButtons",
+					on: {
+						click: {
+							component: "section",
+							variant: "card",
+
+							player_play_button: {
+								component: "switch",
+								text: "playPause"
+							},
+							player_previous_button: {
+								component: "switch",
+								text: "previousVideo"
+							},
+							player_next_button: {
+								component: "switch",
+								text: "nextVideo"
+							},
+							player_volume_button: {
+								component: "switch",
+								text: "volume"
+							},
+							player_autoplay_button: {
+								component: "switch",
+								text: "autoplay"
+							},
+							player_settings_button: {
+								component: "switch",
+								text: "settings"
+							},
+							player_subtitles_button: {
+								component: "switch",
+								text: "subtitles"
+							},
+							player_subtitlesLine_button: {
+								component: "switch",
+								text: "subtitleLine"
+							},
+							player_miniplayer_button: {
+								component: "switch",
+								text: "nativeMiniPlayer"
+							},
+							player_view_button: {
+								component: "switch",
+								text: "viewMode"
+							},
+							player_screen_button: {
+								component: "switch",
+								text: "screen"
+							},
+							player_remote_button: {
+								component: "switch",
+								text: "remote"
+							},
+							player_chaptertitle_button: {
+								component: "switch",
+								text: "chapterTitle"
+							}
 						}
 					}
+				},
+				hide_gradient_bottom: {
+					component: "switch",
+					text: "hideGradientBottom"
+				},
+				always_show_progress_bar: {
+					component: "switch",
+					text: "alwaysShowProgressBar"
+				},
+				player_color: {
+					component: "select",
+					text: "playerColor",
+					options: [{
+						text: "default",
+						value: "default"
+					}, {
+						text: "red",
+						value: "red"
+					}, {
+						text: "pink",
+						value: "pink"
+					}, {
+						text: "purple",
+						value: "purple"
+					}, {
+						text: "deepPurple",
+						value: "deep_purple"
+					}, {
+						text: "indigo",
+						value: "indigo"
+					}, {
+						text: "blue",
+						value: "blue"
+					}, {
+						text: "lightBlue",
+						value: "light_blue"
+					}, {
+						text: "cyan",
+						value: "cyan"
+					}, {
+						text: "teal",
+						value: "teal"
+					}, {
+						text: "green",
+						value: "green"
+					}, {
+						text: "lightGreen",
+						value: "light_green"
+					}, {
+						text: "lime",
+						value: "lime"
+					}, {
+						text: "yellow",
+						value: "yellow"
+					}, {
+						text: "amber",
+						value: "amber"
+					}, {
+						text: "orange",
+						value: "orange"
+					}, {
+						text: "deepOrange",
+						value: "deep_orange"
+					}, {
+						text: "brown",
+						value: "brown"
+					}, {
+						text: "blueGray",
+						value: "blue_gray"
+					}, {
+						text: "white",
+						value: "white"
+					}],
+					tags: "style"
+				},
+				player_transparent_background: {
+					component: "switch",
+					text: "transparentBackground"
+				},
+				player_hide_skip_overlay: {
+					component: "switch",
+					text: "hideSkipOverlay",
+					value: false,
+					tags: "remove,hide"
+				},
+				player_remaining_duration: {
+					component: "switch",
+					text: "showRemainingDuration",
+					id: "show-remaining-duration",
+					value: false
+				},
+				duration_with_speed: {
+					component: "switch",
+					text: "durationWithSpeed",
+					value: false
+				},
+				player_hd_thumbnail: {
+					component: "switch",
+					text: "hdThumbnail",
+					tags: "preview"
+				},
+				hide_scroll_for_details: {
+					component: "switch",
+					text: "hideScrollForDetails",
+					tags: "remove,hide"
+				},
+				hide_top_loading_bar: {
+					component: "switch",
+					text: "hideTopLoadingBar",
+					tags: "remove,hide"
+				},
+			},
+			section_2: {
+				component: 'section',
+				variant: 'card',
+				title: 'Shorts',
+				shorts_always_show_buttons: {
+					component: 'section',
+					variant: 'card',
+					text: 'shortsAlwaysShowButtons',
+					shorts_always_show_captions: {
+						component: 'switch',
+						text: 'shortsAlwaysShowCaptions'
+					},
+					shorts_always_show_fullscreen: {
+						component: 'switch',
+						text: 'shortsAlwaysShowFullscreen'
+					}
 				}
-			},
-			hide_gradient_bottom: {
-				component: "switch",
-				text: "hideGradientBottom"
-			},
-			always_show_progress_bar: {
-				component: "switch",
-				text: "alwaysShowProgressBar"
-			},
-			player_color: {
-				component: "select",
-				text: "playerColor",
-				options: [{
-					text: "default",
-					value: "default"
-				}, {
-					text: "red",
-					value: "red"
-				}, {
-					text: "pink",
-					value: "pink"
-				}, {
-					text: "purple",
-					value: "purple"
-				}, {
-					text: "deepPurple",
-					value: "deep_purple"
-				}, {
-					text: "indigo",
-					value: "indigo"
-				}, {
-					text: "blue",
-					value: "blue"
-				}, {
-					text: "lightBlue",
-					value: "light_blue"
-				}, {
-					text: "cyan",
-					value: "cyan"
-				}, {
-					text: "teal",
-					value: "teal"
-				}, {
-					text: "green",
-					value: "green"
-				}, {
-					text: "lightGreen",
-					value: "light_green"
-				}, {
-					text: "lime",
-					value: "lime"
-				}, {
-					text: "yellow",
-					value: "yellow"
-				}, {
-					text: "amber",
-					value: "amber"
-				}, {
-					text: "orange",
-					value: "orange"
-				}, {
-					text: "deepOrange",
-					value: "deep_orange"
-				}, {
-					text: "brown",
-					value: "brown"
-				}, {
-					text: "blueGray",
-					value: "blue_gray"
-				}, {
-					text: "white",
-					value: "white"
-				}],
-				tags: "style"
-			},
-			player_transparent_background: {
-				component: "switch",
-				text: "transparentBackground"
-			},
-			player_hide_skip_overlay: {
-				component: "switch",
-				text: "hideSkipOverlay",
-				value: false,
-				tags: "remove,hide"
-			},
-			player_remaining_duration: {
-				component: "switch",
-				text: "showRemainingDuration",
-				id: "show-remaining-duration",
-				value: false
-			},
-			duration_with_speed: {
-				component: "switch",
-				text: "durationWithSpeed",
-				value: false
-			},
-			player_hd_thumbnail: {
-				component: "switch",
-				text: "hdThumbnail",
-				tags: "preview"
-			},
-			hide_scroll_for_details: {
-				component: "switch",
-				text: "hideScrollForDetails",
-				tags: "remove,hide"
-			},
-			hide_top_loading_bar: {
-				component: "switch",
-				text: "hideTopLoadingBar",
-				tags: "remove,hide"
-			},
+			}
 		}
 	}
 
-	
+
 };
 
 /*--------------------------------------------------------------
@@ -467,10 +487,10 @@ extension.skeleton.main.layers.section.appearance.on.click.details = {
 				tags: "hide,remove"
 			},
 			disable_likes_animation: {
-		                component: "switch",
-		                text: "disableLikesAnimation",
-		                tags: "likes,animation,disable"
-		        },
+				component: "switch",
+				text: "disableLikesAnimation",
+				tags: "likes,animation,disable"
+			},
 			api: {
 				component: 'section',
 				variant: 'card',
@@ -897,15 +917,15 @@ extension.skeleton.main.layers.section.appearance.on.click.sidebar = {
 					text: "collapsed",
 					value: "collapsed"
 				}, {
-                			text: 'Hide_the_tabs_only',
-		           		value: "hidetabs"
+					text: 'Hide_the_tabs_only',
+					value: "hidetabs"
 				}],
 				tags: "right",
 				on: {
 					click: function () {
 						setTimeout(() => {
-							if (satus.storage.get('related_videos')==="Titles"
-							&& satus.storage.get("relatedVideosPrev") ==="Titles") {
+							if (satus.storage.get('related_videos') === "Titles"
+								&& satus.storage.get("relatedVideosPrev") === "Titles") {
 								if (!satus.storage.get('thumbnails_right')) {
 									this.nextSibling.nextSibling.click();
 									satus.storage.set('relatedVideosPrev', "notTitles")
