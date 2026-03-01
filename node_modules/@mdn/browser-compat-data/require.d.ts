8752b42a0c166120a2f4dd68cb44a72a32ad4c71
tansinhnguyen123 <nsinh2281@gmail.com>
2026-03-01 23:37:36 +1100

    Fix: prevent scroll propagation on playback speed button #3654

---
+++ node_modules/@mdn/browser-compat-data/require.d.ts
@@ -0,0 +1,8 @@
+/* This file is a part of @mdn/browser-compat-data
+ * See LICENSE file for more information. */
+
+import { CompatData } from "./types.js";
+
+declare var bcd: CompatData;
+export default bcd;
+export * from "./types.js";
---
+++ node_modules/eslint-plugin-compat/lib/esm/src/rules/compat.d.ts
@@ -0,0 +1,3 @@
+import { Rule } from "eslint";
+declare const _default: Rule.RuleModule;
+export default _default;
