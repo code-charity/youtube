6ea337555546baae2542d7dfc3b172512f4783aa
ImprovedTube <improvedtube@googlemail.com>
2026-06-11 06:16:48 +0200

    #3915 #3943

--- js&css/web-accessible/www.youtube.com/player.js
+++ js&css/web-accessible/www.youtube.com/player.js
@@ -1067,8 +1067,13 @@ ImprovedTube.playerRotateButton = function () {
 
 				if (rotate == 90 || rotate == 270) {
 					var is_vertical_video = video.videoHeight > video.videoWidth;
-
-					transform += ' scale(' + (is_vertical_video ? video.clientWidth : video.clientHeight) / (is_vertical_video ? video.clientHeight : video.clientWidth) + ')';
+										if (
+												( this.storage.player_cinema_mode_button === true ||  this.storage.player_auto_hide_cinema_mode_when_paused === true ||  this.storage.player_auto_cinema_mode === true
+												) && document.querySelector('#overlay_cinema') && document.querySelector('ytd-app:not([player-fullscreen_]) ytd-watch-flexy:not([fullscreen])')
+											) { transform += ' scale(' + (is_vertical_video ? video.clientWidth : video.clientHeight) / (is_vertical_video ? video.clientHeight : video.clientWidth) + ')';
+													} else {
+											transform += ' scale(' + (is_vertical_video ? player.clientWidth : player.clientHeight) / (is_vertical_video ? player.clientHeight : player.clientWidth) + ')';
+										}					
 				}
 
 				if (!ImprovedTube.elements.buttons['it-rotate-styles']) {
